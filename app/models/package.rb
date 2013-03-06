ceae77ba221e128d4218554bb5dc257e7dd64a42
lilu <lilu.life@gmail.com>
2013-03-06 16:56:15 +0800

    develop thing package

--- app/models/package.rb
+++ app/models/package.rb
@@ -4,6 +4,7 @@ class Package
   field :title, type: String, default: ""
   field :shop,  type: String, default: ""
   field :price, type: Float,  default: 0
+  field :stock, type: Integer, default: 0
   mount_uploader :photo, ImageUploader
   embedded_in :thing
 
