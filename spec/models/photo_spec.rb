57de600bb9f012bf37e016d85d003e421c3b2232
lilu <lilu.life@gmail.com>
2013-03-12 20:47:34 +0800

    some clean up for specs

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
 describe Photo do
-  before do
-    @photo = Fabricate(:photo)
-  end
-
-  subject {@photo}
-
-  it {should be_valid}
-  its(:image_url) {should =~ Regexp.new(Settings.upyun.photo_bucket_domain)}
-  its(:name) {should == "example.png"}
-  its(:size) {should == 1479}
-
 end
