15491662d9d302063d5b0be004bbcbd1b22ffb2f
jasl <jasl9187@hotmail.com>
2013-09-05 04:54:32 +0800

    remove multiple remove cart

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -4,10 +4,6 @@ module CartsHelper
     link_to '结算', new_order_path, class: 'btn btn-large btn-success place-order', disabled: @has_stock_items.empty?
   end
 
-  def delete_multiple_link
-    link_to '删除', 'javascript:void(0)', class: 'delete-multiple disabled', disabled: true
-  end
-
   def cart_items_count
     current_user.cart_items.size
   end
