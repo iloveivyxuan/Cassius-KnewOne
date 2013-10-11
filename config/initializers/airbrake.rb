7a77e795e3a1fdec34b2155b2fae170a84853d7e
jasl <jasl9187@hotmail.com>
2013-10-11 16:51:14 +0800

    airbrake only enable on production

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,5 @@
-Airbrake.configure do |config|
-  config.api_key = 'c7ac118b281cc0cb99161502acb712e3'
+if Rails.env.production?
+  Airbrake.configure do |config|
+    config.api_key = 'c7ac118b281cc0cb99161502acb712e3'
+  end
 end
