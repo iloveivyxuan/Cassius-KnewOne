d70e57a017ef8b5838ff75c756f675a2e259eadb
jasl <jasl9187@hotmail.com>
2013-12-13 03:39:16 +0800

    fix send mail on real env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,4 +79,5 @@ Making::Application.configure do
   }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'staging.knewone.com' }
 end
