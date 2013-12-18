17565ea0776907268c75e415398f411647d0fced
jasl <jasl9187@hotmail.com>
2013-12-19 03:46:54 +0800

    move tenpay setting to application.yml for test easily

--- config/initializers/tenpay.rb
+++ config/initializers/tenpay.rb
@@ -2,5 +2,5 @@
 #JaslTenpay.pid = '1217165001'
 #JaslTenpay.key = 'a30a93a07e7d72c8aba2972011ce63a5'
 
-JaslTenpay.pid = '1217836601'
-JaslTenpay.key = '7130fa61604c9926418dbe140ccaaf19'
+JaslTenpay.pid = Settings.tenpay.pid
+JaslTenpay.key = Settings.tenpay.key
