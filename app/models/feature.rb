a34e7dcfa7fa4c22bafe8b2bb6b4eeaa566d3e95
lilu <lilu.life@gmail.com>
2013-12-23 17:14:23 +0800

    revert feature model and change navbar user nav

---
+++ app/models/feature.rb
@@ -0,0 +1,10 @@
+class Feature < Post
+  field :priority, type: Integer, default: 0
+
+  include Fancyable
+  has_and_belongs_to_many :fanciers, class_name: "User", inverse_of: :fancy_features
+
+  default_scope desc(:priority)
+
+  belongs_to :thing
+end
