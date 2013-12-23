2b3907826ae16d429b18361ab32084e14139755c
lilu <lilu.life@gmail.com>
2013-12-23 16:31:42 +0800

    upgrade thing show

--- app/presenters/thing_presenter.rb
+++ app/presenters/thing_presenter.rb
@@ -43,6 +43,13 @@ class ThingPresenter < PostPresenter
     end
   end
 
+  def content_for_price
+    price_text = price
+    if price_text.present?
+      content_tag :div, price_text, class: "price"
+    end
+  end
+
   def shopping_desc
     return if thing.shopping_desc.blank?
     summary = strip_tags(thing.shopping_desc).truncate(48).html_safe
@@ -51,20 +58,20 @@ class ThingPresenter < PostPresenter
   end
 
   def concept
-    link_to_with_icon "研发中", "fa fa-wrench fa-lg", "#",
-                      title: "概念产品", class: "btn btn-default disabled popover-toggle",
-                      data: {
-                          toggle: "popover",
-                          placement: "bottom",
-                          content: "由于产品还在研发之中，目前还没有合适的渠道让您购买到此商品，不过，我们会一直追踪此商品的最新动向，一旦您所在的地区可以购买，我们会第一时间提供最靠谱的购买渠道，敬请期待"
-                      }
+    # link_to_with_icon "研发中", "fa fa-wrench fa-lg", "#",
+    #                   title: "概念产品", class: "btn btn-default disabled popover-toggle",
+    #                   data: {
+    #                       toggle: "popover",
+    #                       placement: "bottom",
+    #                       content: "由于产品还在研发之中，目前还没有合适的渠道让您购买到此商品，不过，我们会一直追踪此商品的最新动向，一旦您所在的地区可以购买，我们会第一时间提供最靠谱的购买渠道，敬请期待"
+    #                   }
   end
 
   def domestic
     return concept unless thing.shop.present?
 
     link_to_with_icon "网购", "fa fa-location-arrow fa-lg", buy_thing_path(thing),
-                      title: title, class: "btn btn-info track_event", target: "_blank",
+                      title: title, class: "btn btn-info buy_button track_event", target: "_blank",
                       data: {
                           action: "buy",
                           category: "domestic",
@@ -76,59 +83,11 @@ class ThingPresenter < PostPresenter
     return concept unless thing.shop.present?
 
     link_to_with_icon "海淘", "fa fa-plane fa-lg", buy_thing_path(thing),
-                      title: title, class: "btn btn-info track_event", target: "_blank",
+                      title: title, class: "btn btn-info buy_button track_event", target: "_blank",
                       data: {
                           action: "buy",
                           category: "abroad",
                           label: title
-                      }
-  end
-
-  def presell
-    return concept unless thing.shop.present?
-
-    link_to_with_icon "预购", "fa fa-phone fa-lg", buy_thing_path(thing),
-                      title: title, class: "btn btn-warning track_event", target: "_blank",
-                      data: {
-                          action: "buy",
-                          category: "presell",
-                          label: title
-                      }
-  end
-
-  def ship
-    return concept unless thing.shop.present?
-
-    link_to_with_icon "即将到货", "fa fa-anchor fa-lg", "#",
-                      title: "断货产品", class: "btn btn-success disabled popover-toggle",
-                      data: {
-                          toggle: "popover",
-                          placement: "bottom",
-                          content: "十分抱歉，我们当前没有此产品的库存，不过，一旦新一批产品到货，我们将会通知每位喜欢此产品的用户"
-                      }
-  end
-
-  def stock
-    return concept unless thing.shop.present?
-
-    link_to_with_icon "购买", "fa fa-shopping-cart fa-lg", buy_thing_path(thing),
-                      title: title, class: "btn btn-success track_event", target: "_blank",
-                      data: {
-                          action: "buy",
-                          category: "stock",
-                          label: title
-                      }
-  end
-
-  def exclusive
-    return concept unless thing.shop.present?
-
-    link_to_with_icon "限量", "fa fa-credit-card fa-lg", "#",
-                      title: "限量产品", class: "btn btn-inverse disabled popover-toggle",
-                      data: {
-                          toggle: "popover",
-                          placement: "bottom",
-                          content: "此产品数量非常有限，因此我们只提供给少数最狂热的爱好者，敬请谅解"
                       }
   end
 
@@ -148,12 +107,19 @@ class ThingPresenter < PostPresenter
     end
   end
 
+  def content_for_buy
+    buy_text = buy
+    if buy_text.present?
+      content_tag :div, buy_text, class: "buy"
+    end
+  end
+
   def official_site
     if thing.official_site.present?
       if thing.official_site !~ /^https?:\/\//
         thing.official_site = "http://#{thing.official_site}"
       end
-      link_to_with_icon "", "fa fa-globe", thing.official_site, target: "_blank", title: "官方信息"
+      link_to_with_icon "官方网站", "fa-li fa fa-globe", thing.official_site, target: "_blank", title: "官方信息"
     end
   end
 
@@ -191,15 +157,15 @@ class ThingPresenter < PostPresenter
     if current_user == thing.author
       "我在#{topic}上分享了一个酷产品, #{title}: #{thing_url(thing, :refer => 'weibo')}"
     else
-      str = "我在#{topic}发现了一个酷产品, #{title}: #{thing_url(thing, :refer => 'weibo')}"
-      if current_user.current_auth && current_user != thing.author
-        if current_user.equal_auth_provider?(thing.author)
-          str += " (感谢 @#{thing.author.name} 分享)"
-        elsif thing.author.current_auth
-          str += " (感谢 #{thing.author.name} from #{thing.author.provider} 分享)"
-        end
-      end
-      str
+      "我在#{topic}发现了一个酷产品, #{title}: #{thing_url(thing, :refer => 'weibo')}"
+      # TODO: Thanks user depend on provider
+      # if current_user.current_auth && current_user != thing.author
+      #   if current_user.equal_auth_provider?(thing.author)
+      #     str += " (感谢 @#{thing.author.name} 分享)"
+      #   elsif thing.author.current_auth
+      #     str += " (感谢 #{thing.author.name} from #{thing.author.provider} 分享)"
+      #   end
+      # end
     end
   end
 
