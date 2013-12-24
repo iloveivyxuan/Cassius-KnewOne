07c6560934f62e44abb56b520a912b90290f955c
jasl <jasl9187@hotmail.com>
2013-12-24 17:02:23 +0800

    temp disable async mailer

--- config/initializers/devise_async.rb
+++ config/initializers/devise_async.rb
@@ -1,5 +1,6 @@
 Devise::Async.setup do |config|
-  config.enabled = Rails.env.production? ? true : false
+  # config.enabled = Rails.env.production? ? true : false
+  config.enabled = false
   config.backend = :sidekiq
   config.queue = :mails
 end
