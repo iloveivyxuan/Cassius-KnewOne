5eca1344476aa676b4b2333ef7c3441a6fcfb46e
lilu <lilu.life@gmail.com>
2013-12-31 14:43:28 +0800

    show kinds info on thing kinds

--- app/presenters/kind_presenter.rb
+++ app/presenters/kind_presenter.rb
@@ -1,11 +1,18 @@
+# -*- coding: utf-8 -*-
 class KindPresenter < ApplicationPresenter
   presents :kind
-  delegate :title, :id, :price, :photo_number, :stock, :max_per_buy, to: :kind
+  delegate :title, :id, :photo_number, :stock, :max_per_buy, to: :kind
 
   def estimated_at
     if kind.stage == :ship and kind.estimates_at.present? and kind.estimates_at > Time.now
       time_tag kind.estimates_at, distance_of_time_in_words_to_now(kind.estimates_at)
     end.try(:gsub, "\"", "'")
+  end
+
+  def content_for_estimated_at
+    e = estimated_at
+    return if e.blank?
+    "预估将于 #{e} 后发货 ".html_safe
   end
 
   def max
@@ -14,11 +21,19 @@ class KindPresenter < ApplicationPresenter
     end.min
   end
 
+  def price
+    price_format(kind.price)
+  end
+
+  def limit
+    "<em>#{kind.stock-kind.sold}</em> 库存".html_safe
+  end
+
   def build_option
     [title, id, data: {
          stock: stock,
          max: max,
-         price: price_format(price),
+         price: price,
          photo: photo_number,
          estimated: estimated_at
      }, class: "kind_option", disabled: (stock <= 0)]
