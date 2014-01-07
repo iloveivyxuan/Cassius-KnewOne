e506b96329d38eee351baf670a6ac43d162b2f47
jasl <jasl9187@hotmail.com>
2014-01-08 01:22:35 +0800

    fix wrong email regex

--- lib/devise_backgrounder.rb
+++ lib/devise_backgrounder.rb
@@ -13,18 +13,18 @@ class DeviseBackgrounder
   end
 
   def initialize(method, record, token, opts = {})
-    @method, @record, @token, @opts = method, record, token, opts
+    @method, @record_id, @token, @opts = method, record.id.to_s, token, opts
   end
 
   if Rails.env.development?
     def deliver
-      DeviseMailer.send(@method, @record.id, @token, @opts).deliver
+      DeviseMailer.send(@method, @record_id, @token, @opts).deliver
     end
   else
     def deliver
       # You need to hardcode the class of the Devise mailer that you
       # actually want to use. The default is Devise::Mailer.
-      DeviseMailer.delay.send(@method, @record.id, @token, @opts)
+      DeviseMailer.delay.send(@method, @record_id, @token, @opts)
     end
   end
 end
