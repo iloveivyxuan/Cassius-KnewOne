63ad090b143d1b1e0866bc24ee536bf4e7211579
jasl <jasl9187@hotmail.com>
2014-02-12 03:28:27 +0800

    normal user add 10s comment cooldown

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,15 @@ class CommentsController < ApplicationController
   respond_to :json
   load_and_authorize_resource :post
 
+  after_action only: :create do
+    session[:cooldown] = Time.now.to_i
+  end
+
+  before_action only: :create do
+    return if current_user.staff?
+    head :not_acceptable if session[:cooldown].present? && Time.now.to_i < (session[:cooldown].to_i + 10)
+  end
+
   def index
     @comments = @post.comments.page(params[:page]).per(Settings.comments.per_page)
     respond_with @comments
