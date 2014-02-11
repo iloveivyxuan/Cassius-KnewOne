94c28cc68a755d1ad0de61fc843605f3cf186d48
jasl <jasl9187@hotmail.com>
2014-02-11 22:03:12 +0800

    improve devilier_tip

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -4,4 +4,12 @@ module CartsHelper
     count = current_user.cart_items.size
     (count > 0) ? count : ""
   end
+
+  def deliver_tip_text(total_price)
+    if (gap = 500 - total_price) > 0
+      "(已含运费，您仅还需 <strong class=\"deliver_free_gap\">#{gap}</strong> 元即可免费升级至顺丰速运)".html_safe
+    else
+      "(已含运费，使用顺丰速运)"
+    end
+  end
 end
