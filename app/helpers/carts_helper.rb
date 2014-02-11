b7f6dde4ec7e3d3484ad582df4ce7255683067ac
jasl <jasl9187@hotmail.com>
2014-02-11 22:10:30 +0800

    fix deliver_tip

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -6,6 +6,8 @@ module CartsHelper
   end
 
   def deliver_tip_text(total_price)
+    return '' if total_price == 0
+
     if (gap = 500 - total_price) > 0
       "(已含运费，您仅还需 <strong class=\"deliver_free_gap\">#{gap}</strong> 元即可免费升级至顺丰速运)".html_safe
     else
