69165aa81c6de259f0f2edce9740c4205356e927
jasl <jasl9187@hotmail.com>
2014-02-14 02:33:18 +0800

    update deliver tips

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -9,9 +9,9 @@ module CartsHelper
     return '' if total_price == 0
 
     if (gap = 500 - total_price) > 0
-      "*总价已包含运费(中通快递)，再选购 <strong class=\"deliver_free_gap\">#{gap}</strong> 元的产品即可免费升级至顺丰速运。".html_safe
+      "*总价已含运费(中通快递)，再选购 <strong class=\"deliver_free_gap\">#{gap}</strong> 元的产品即可免费升级至顺丰速运(或补10元)。".html_safe
     else
-      '*已含运费(顺丰速运)。'
+      '*总价已含运费(顺丰速运)。'
     end
   end
 end
