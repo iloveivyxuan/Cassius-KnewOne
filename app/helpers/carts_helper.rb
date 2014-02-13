62583e03a53e0c0b6e85f012b16754b13b26272d
lilu <lilu.life@gmail.com>
2014-02-13 13:43:12 +0800

    cart item deliver tips update

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -9,7 +9,7 @@ module CartsHelper
     return '' if total_price == 0
 
     if (gap = 500 - total_price) > 0
-      "(已含运费，您仅还需 <strong class=\"deliver_free_gap\">#{gap}</strong> 元即可免费升级至顺丰速运)".html_safe
+      "(已含运费，您还需要再选购 <strong class=\"deliver_free_gap\">#{gap}</strong> 元的产品即可免费升级至顺丰速运)".html_safe
     else
       "(已含运费，使用顺丰速运)"
     end
