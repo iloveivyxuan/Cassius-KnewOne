fce8d144abb126ce35ffa44c80593c363387c915
jasl <jasl9187@hotmail.com>
2014-02-14 19:26:13 +0800

    Auth uid using string

--- app/models/auth.rb
+++ app/models/auth.rb
@@ -2,7 +2,7 @@
 class Auth
   include Mongoid::Document
   field :provider, type: String
-  field :uid, type: Integer
+  field :uid, type: String
   field :name, type: String
   field :access_token, type: String
   field :access_token_secret, type: String
