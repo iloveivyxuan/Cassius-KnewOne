47b490462ddbc73299eec509688f7a20d6edc57c
jasl <jasl9187@hotmail.com>
2014-02-19 14:50:26 +0800

    multi smtp server

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,37 +1,73 @@
 class BaseMailer < ActionMailer::Base
   abstract!
   layout 'mailer'
-  default from: 'welcome@knewone.com',
-          content_type: 'text/html',
+  default content_type: 'text/html',
           reply_to: 'welcome@knewone.com'
+
+  #SMTP_SERVERS = {
+  #    white: {
+  #        from: 'welcome@knewone.com',
+  #        delivery_settings: {
+  #            address: 'smtp.exmail.qq.com',
+  #            port: 25,
+  #            domain: 'knewone.com',
+  #            user_name: 'welcome@knewone.com',
+  #            password: 'knewone123',
+  #            authentication: 'plain',
+  #            enable_starttls_auto: true
+  #        }
+  #    },
+  #    default: {
+  #        from: 'welcome@service.knewone.com',
+  #        delivery_settings: {
+  #            address: 'smtpcloud.sohu.com',
+  #            port: 25,
+  #            user_name: 'postmaster@knewone-service.sendcloud.org',
+  #            password: 'q4yoSPmu',
+  #            authentication: 'login',
+  #            domain: 'sendcloud.org'
+  #        }
+  #    }
+  #}
 
   SMTP_SERVERS = {
       white: {
-          address: 'smtp.exmail.qq.com',
-          port: 25,
-          domain: 'knewone.com',
-          user_name: 'welcome@knewone.com',
-          password: 'knewone123',
-          authentication: 'plain',
-          enable_starttls_auto: true
+          from: 'welcome@knewone.com',
+          delivery_settings: {
+              address: 'smtp.exmail.qq.com',
+              port: 25,
+              domain: 'knewone.com',
+              user_name: 'welcome@knewone.com',
+              password: 'knewone123',
+              authentication: 'plain',
+              enable_starttls_auto: true
+          }
       },
       default: {
-          address: 'smtp.exmail.qq.com',
-          port: 25,
-          domain: 'knewone.com',
-          user_name: 'welcome@knewone.com',
-          password: 'knewone123',
-          authentication: 'plain',
-          enable_starttls_auto: true
+          from: 'welcome@knewone.com',
+          delivery_settings: {
+              address: 'smtp.exmail.qq.com',
+              port: 25,
+              domain: 'knewone.com',
+              user_name: 'welcome@knewone.com',
+              password: 'knewone123',
+              authentication: 'plain',
+              enable_starttls_auto: true
+          }
       }
   }
 
-  WHITES = %w(gmail.com)
+  WHITES = %w(gmail.com ruby-china.org)
 
   def mail(headers = {}, &block)
-    m = super headers, &block
-    m.delivery_method.settings.merge!(route_smtp_server(m.to.first)) if self.delivery_method == :smtp
-    m
+    if self.delivery_method == :smtp
+      smtp_config = route_smtp_server(headers[:to])
+      m = super headers.merge(from: smtp_config[:from]), &block
+      m.delivery_method.settings.merge!(smtp_config[:delivery_settings])
+      m
+    else
+      super headers, &block
+    end
   end
 
   private
