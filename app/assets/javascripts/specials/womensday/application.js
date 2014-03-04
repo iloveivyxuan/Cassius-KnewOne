58be46d32bc01ff6955dab275310164719ce92a1
Syn Zhang <zeyanzhang.china@gmail.com>
2014-03-04 18:46:11 +0800

    enable shake animation on touch event on touch device

--- app/assets/javascripts/specials/womensday/application.js
+++ app/assets/javascripts/specials/womensday/application.js
@@ -4,8 +4,16 @@
 //= require ../common
 
 $(function() {
+  var $easteregg = $('#easteregg');
+
   $('#social_share_wechat').popover({
     html: true,
     content: $('<div />').qrcode({render: 'image', ecLevel: 'L', text: document.URL})
+  });
+
+  $(window).on('touchstart', function() {
+    $easteregg.addClass('shaking');
+  }).on('touchend', function() {
+    $easteregg.removeClass('shaking');
   });
 });
