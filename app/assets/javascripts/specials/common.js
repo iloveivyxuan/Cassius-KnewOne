004b322304e47982a74410c2beea40217d84cd3b
Syn Zhang <zeyanzhang.china@gmail.com>
2014-03-04 11:48:49 +0800

    add social share icon & demand loading lark

--- app/assets/javascripts/specials/common.js
+++ app/assets/javascripts/specials/common.js
@@ -1,36 +1,62 @@
-var Making = (function(module) {
-  module.OlarkSetUser = function(name, email, id) {
-    if (typeof name != 'undefined') {
-      olark('api.visitor.updateFullName', {fullName: name});
-      olark('api.chat.updateVisitorNickname', {snippet: name });
-    }
-    if (typeof email != 'undefined')
-      olark('api.visitor.updateEmailAddress', {emailAddress: email});
-    if (typeof id != 'undefined')
-      olark('api.visitor.updateCustomFields', {customerId: id});
+window.Making = (function(module) {
+  module.SetupOlark = function(element) {
+    var $element    = $(element),
+        klass_hint  = $element.data('hint'),
+        klass_spin  = 'fa-spinner fa-spin',
+        $hint       = $element.find('.' + klass_hint),
+        initialized = false;
+
+    $element.on('click', function(event) {
+      event.preventDefault();
+
+      if (!initialized) {
+        if ($hint.length > 0) {
+          $hint.removeClass(klass_hint).addClass(klass_spin);
+        }
+
+        $.getScript($element.data('url'), function() {
+          var name  = $element.data('name'),
+              email = $element.data('email'),
+              id    = $element.data('id');
+
+          if (name) {
+            olark('api.visitor.updateFullName', {fullName: name});
+            olark('api.chat.updateVisitorNickname', {snippet: name });
+          }
+          if (email) olark('api.visitor.updateEmailAddress', {emailAddress: email});
+          if (id) olark('api.visitor.updateCustomFields', {customerId: id});
+
+          olark('api.box.expand');
+
+          if ($hint.length > 0) {
+            olark('api.box.onExpand', function() {
+              $hint.removeClass(klass_spin).addClass(klass_hint);
+            });
+          }
+
+          initialized = true;
+        });
+      } else {
+        olark('api.box.expand');
+      }
+    });
   };
 
   return module;
 })(window.Making || {});
 
 (function() {
-  var feedback = document.getElementById('feedback'),
-      display  = 'none';
+  var $feedback = $('#feedback');
 
-  feedback.addEventListener('click', function(event) {
-    event.preventDefault();
-    olark('api.box.expand');
-  }, false);
+  window.Making.SetupOlark('#feedback');
 
   if (Modernizr.mq('(max-width: 768px)')) {
-    window.addEventListener('scroll', function() {
-      if (document.documentElement.scrollTop > 400) {
-        display = 'block';
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() > 400) {
+        $feedback.show();
       } else {
-        display = 'none';
+        $feedback.hide();
       }
-
-      feedback.style.display = display;
-    }, false);
+    });
   }
 })();
--- app/assets/javascripts/specials/womensday/application.js
+++ app/assets/javascripts/specials/womensday/application.js
@@ -1,1 +1,8 @@
+//= require jquery
+//= require bootstrap
+//= require jquery.qrcode
 //= require ../common
+
+$(function() {
+  $('.qr-field').qrcode({render: 'image', ecLevel: 'L', text: document.URL});
+});
