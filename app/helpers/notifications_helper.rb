a0c6527f1a13743aefc90101b4762ff8ee2bd411
Syn Zhang <zeyanzhang.china@gmail.com>
2014-03-11 19:16:55 +0800

    add notification box on header

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,7 +2,7 @@
 module NotificationsHelper
   def senders(message)
     links = message.senders.map do |sender|
-      link_to sender.name, sender, class: "btn btn-link"
+      link_to sender.name, sender
     end.take(5).join('，')
 
     if message.senders.count > 5
@@ -15,13 +15,13 @@ module NotificationsHelper
     content = ""
     case post.class
       when Thing then
-        content += "产品"
-        content += link_to present(post).full_title, thing_path(post)
+        content += "产品 "
+        content += link_to post.title, thing_path(post)
       when Review then
-        content += "评测"
+        content += "评测 "
         content += link_to post.title, thing_review_path(post.thing, post)
       when Topic then
-        content += "帖子"
+        content += "帖子 "
         content += link_to post.title, group_topic_path(post.group, post)
       else
         content += '失效的资源'
@@ -48,11 +48,11 @@ module NotificationsHelper
     html = ''
     html += senders(notification)
     if notification.context.nil?
-      html += '回复了已经被删除的资源'
+      html += ' 回复了已经被删除的资源'
     elsif notification.context.author == current_user
-      html += "回复了我发布的#{notification_post(notification.context)}"
+      html += " 回复了我发布的#{notification_post(notification.context)}"
     else
-      html += "在对#{notification_post(notification.context)}的回复中提到了我"
+      html += " 在对#{notification_post(notification.context)} 的回复中提到了我"
     end
     html.html_safe
   end
@@ -63,9 +63,9 @@ module NotificationsHelper
 
     html += senders(notification)
     if notification.context.author == current_user
-      html += '为我分享的产品发表了评测'
+      html += ' 为我分享的产品发表了评测 '
     else
-      html += '为我喜欢的产品发表了评测'
+      html += ' 为我喜欢的产品发表了评测 '
     end
     html += link_to notification.context.title, thing_review_path(notification.context.thing, notification.context)
 
@@ -76,7 +76,7 @@ module NotificationsHelper
     html = ''
 
     html += link_to notification.context.title, notification.context
-    html += '有现货'
+    html += ' 有现货'
 
     html.html_safe
   end
