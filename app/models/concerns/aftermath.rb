f3dc2e23c1aac7a9c9e78da299aa4ddea73e6083
jasl <jasl9187@hotmail.com>
2014-03-16 19:24:27 +0800

    support rails magic method

--- app/models/concerns/aftermath.rb
+++ app/models/concerns/aftermath.rb
@@ -2,13 +2,20 @@ module Aftermath
   extend ActiveSupport::Concern
 
   module ClassMethods
+    RAILS_MAGIC_METHODS = [:create, :save, :update]
     # only a true value can be triggered
     def need_aftermath(*args)
       args.each do |m|
-        send :alias_method, :"_#{m}", m
-        define_method m do |*args|
-          if send :"_#{m}", *args
-            AftermathHandler.send :"#{self.model_name.underscore}_#{m.to_s}", self
+        if RAILS_MAGIC_METHODS.include? m
+          send :"after_#{m}" do
+            AftermathHandler.send :"#{self.model_name.to_s.underscore}_#{m.to_s}", self
+          end
+        else
+          send :alias_method, :"_#{m}", m
+          define_method m do |*args|
+            if send :"_#{m}", *args
+              AftermathHandler.send :"#{self.model_name.underscore}_#{m.to_s}", self
+            end
           end
         end
       end
