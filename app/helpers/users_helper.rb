ee56f33e4682198d672abc46922b561fe589446f
jasl <jasl9187@hotmail.com>
2014-03-17 06:24:56 +0800

    refector users layout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,12 +4,6 @@ module UsersHelper
     image_tag user.avatar.url(size), alt: user.name
   end
 
-  def user_location(user)
-    location = user.location
-    return nil if location.blank?
-    location
-  end
-
   def provider_sync
     return nil unless current_user && current_user.current_auth
     raw "<label id='check_provider_sync' class='checkbox inline'><input type='checkbox' name='provider_sync' checked>分享到社交网络</label>"
@@ -51,12 +45,33 @@ module UsersHelper
     user.current_auth.topic_wrapper topic
   end
 
+  def render_user_links(user)
+    html = user_links(user)
+    return if html.blank?
+
+    content_tag :dd, nil, class: 'url', title: '社交网络', itemprop: 'url' do
+      content_tag(:label, nil) { content_tag(:i, nil, class: 'fa fa-globe') } +
+          content_tag(:div, nil, class: 'data') { user_links(user) }
+    end
+  end
+
+  def render_user_location(user)
+    return if user.location.blank?
+
+    content_tag :dd, nil, class: 'location', title: '地区' do
+      content_tag(:label, nil) { content_tag(:i, nil, class: 'fa fa-map-marker') } +
+          content_tag(:address, user.location, class: 'data')
+    end
+  end
+
   def render_user_gender(user)
     if user.gender.present?
       content_tag :small, class: 'gender' do
         case user.gender
-          when '男' then '♂'
-          when '女' then '♀'
+          when '男' then
+            '♂'
+          when '女' then
+            '♀'
         end
       end
     end
