5b25567379de42df9d624642ce77320ec61c36fb
jasl <jasl9187@hotmail.com>
2014-03-17 15:55:49 +0800

    bug fix, prevent count below 0

--- app/models/aftermath_handler.rb
+++ app/models/aftermath_handler.rb
@@ -16,31 +16,31 @@ class AftermathHandler
     def thing_destroy(thing)
       u = thing.author
 
-      u.inc things_count: -1
+      u.inc things_count: -1 if user.things_count > 0
     end
 
     def thing_fancy(thing, user)
       user.inc fancies_count: 1
 
-      user.category_references<< thing.categories_text
+      user.category_references<< thing.categories
     end
 
     def thing_unfancy(thing, user)
-      user.inc fancies_count: -1
+      user.inc fancies_count: -1 if user.fancies_count > 0
 
-      user.category_references>> thing.categories_text
+      user.category_references>> thing.categories
     end
 
     def thing_own(thing, user)
       user.inc owns_count: 1
 
-      user.category_references<< thing.categories_text
+      user.category_references<< thing.categories
     end
 
     def thing_unown(thing, user)
-      user.inc owns_count: -1
+      user.inc owns_count: -1 if user.owns_count > 0
 
-      user.category_references>> thing.categories_text
+      user.category_references>> thing.categories
     end
 
     def review_create(review)
@@ -52,7 +52,7 @@ class AftermathHandler
     def review_destroy(review)
       u = review.author
 
-      u.inc reviews_count: -1
+      u.inc reviews_count: -1 if user.reviews_count > 0
     end
 
     def user_follow(record, user)
@@ -61,15 +61,15 @@ class AftermathHandler
     end
 
     def user_unfollow(record, user)
-      record.inc followings_count: -1
-      user.inc followers_count: -1
+      record.inc followings_count: -1 if user.followings_count > 0
+      user.inc followers_count: -1 if user.followers_count > 0
     end
 
     def order_confirm_payment!(order)
       u = order.user
 
       if order.payment_method != :btc
-        u.inc expenses_count: order.trade_price,
+        u.inc expenses_count: order.trade_price.to_i,
               orders_count: 1
       else
         u.inc orders_count: 1
