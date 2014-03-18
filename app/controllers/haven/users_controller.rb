94afff5659443aad2d1c0f434676abba88effb56
jasl <jasl9187@hotmail.com>
2014-03-18 14:54:50 +0800

    add haven#users/index followings followers filter

--- app/controllers/haven/users_controller.rb
+++ app/controllers/haven/users_controller.rb
@@ -22,6 +22,15 @@ module Haven
         if params[:filter].include? 'expense'
           @users = @users.where(:expenses_count.gt => 0).order_by([:expenses_count, :desc])
         end
+        if params[:filter].include? 'followings'
+          @users = @users.where(:followings_count.gt => 0).order_by([:followings_count, :desc])
+        end
+        if params[:filter].include? 'followers'
+          @users = @users.where(:followers_count.gt => 0).order_by([:followers_count, :desc])
+        end
+        if params[:filter].include? 'expense'
+          @users = @users.where(:expenses_count.gt => 0).order_by([:expenses_count, :desc])
+        end
         if params[:filter].include? 'unbind_email'
           @users = @users.where(:email => nil, :unconfirmed_email => nil).order_by([:created_at, :asc])
         end
