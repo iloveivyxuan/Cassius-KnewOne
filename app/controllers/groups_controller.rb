b26c469ba9ba608601d601578a5df4f0107e62c8
lilu <lilu.life@gmail.com>
2014-03-22 17:36:08 +0800

    begin developing groups index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @groups = Group.all
+    @groups = Group.desc(:members_count).limit(10)
   end
 
   def show
