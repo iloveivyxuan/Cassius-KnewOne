92cc94c8e8ee6cf4d6752cbd26f6f8ec6b62d841
jasl <jasl9187@hotmail.com>
2014-04-01 04:33:59 +0800

    init search

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,8 @@ module HomeHelper
   def home_nav(action, options = {}, &block)
     li((action_name == action.to_s), options, &block)
   end
+
+  def search_nav(type, default = false, options = {}, &block)
+    li(((params[:type] == type) || (default && params[:type].blank?)), options, &block)
+  end
 end
