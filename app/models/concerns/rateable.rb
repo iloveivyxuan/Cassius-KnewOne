ee793f431d955cac37383326de56bfcacf74ae30
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-04-21 20:45:44 +0800

    Simplify validation of score

--- app/models/concerns/rateable.rb
+++ app/models/concerns/rateable.rb
@@ -3,11 +3,6 @@ module Rateable
 
   included do
     field :score, type: Integer, default: 0
-
-    validate do |obj|
-      unless (0..5).include? obj.score
-        errors.add :score, "Score should be in [0..5]"
-      end
-    end
+    validates :score, inclusion: { in: 0..5 }
   end
 end
