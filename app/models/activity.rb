84c46ed05d98cd8c7d5118afeac8e728cb323e23
lilu <lilu.life@gmail.com>
2014-04-22 14:55:08 +0800

    refractor activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,16 +2,29 @@ class Activity
   include Mongoid::Document
   include Mongoid::Timestamps::Created
 
-  belongs_to :user
-  index({user_id: 1})
-
-  field :visible, type: Boolean, default: true
-
   field :type, type: Symbol
   validates :type, presence: true
 
   field :reference_union, type: String
   index({reference_union: 1})
+
+  field :source_union, type: String
+  index({source_union: 1})
+
+  field :visible, type: Boolean, default: true
+
+  belongs_to :user
+  index({user_id: 1})
+
+  default_scope -> { desc(:created_at) }
+
+  scope :visible, -> { where visible: true }
+  scope :by_users, ->(users) { where(:user_id.in => users.map(&:id)) }
+  scope :by_type, ->(type) { where type: type }
+  scope :by_types, ->(*types) { where :type.in => types }
+  scope :by_reference, ->(record) do
+    where reference_union: "#{record.class.name}_#{record.id.to_s}"
+  end
 
   def reference
     return if self.reference_union.blank?
@@ -24,9 +37,6 @@ class Activity
     self.reference_union = "#{record.class.to_s}_#{record.id.to_s}"
   end
 
-  field :source_union, type: String
-  index({source_union: 1})
-
   def source
     return if self.source_union.blank?
 
@@ -37,15 +47,6 @@ class Activity
   def source=(record)
     self.source_union = "#{record.class.to_s}_#{record.id.to_s}"
   end
-
-  default_scope -> { desc(:created_at) }
-
-  scope :visible, -> { where visible: true }
-  scope :by_users, ->(users) { where(:user_id.in => users.map(&:id)) }
-  scope :by_reference,
-        ->(record) { where reference_union: "#{record.class.name}_#{record.id.to_s}" }
-  scope :by_type, ->(type) { where type: type }
-  scope :by_types, ->(*types) { where :type.in => types }
 
   def identifier
     [source_union, reference_union].compact.join('_')
