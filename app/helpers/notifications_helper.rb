d7bdc175c0a70719710e9c6dcd2ffeb6c0f57ca1
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-04-30 11:17:05 +0800

    Fix notification to feelings

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -20,6 +20,9 @@ module NotificationsHelper
       when Review then
         content += "评测"
         content += link_to post.title, thing_review_path(post.thing, post), target: target
+      when Feeling then
+        content += "短评"
+        content += link_to post.title, thing_feeling_path(post.thing, post), target: target
       when Topic then
         content += "帖子"
         content += link_to post.title, group_topic_path(post.group, post), target: target
