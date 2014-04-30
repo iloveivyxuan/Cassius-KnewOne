ce21fa66087fd8828496b647fa47a6be57e68fd1
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-04-30 11:38:32 +0800

    Add Post#unvote

--- app/models/post.rb
+++ app/models/post.rb
@@ -36,6 +36,18 @@ class Post
     self.update_attribute :lovers_count, lovers.count
   end
 
+  def unvote(user, love)
+    return unless voted?(user)
+    if love
+      lovers.delete(user)
+      author.inc karma: -Settings.karma.post
+    else
+      foes.delete(user)
+      author.inc karma: Settings.karma.post
+    end
+    self.update_attribute :lovers_count, lovers.count
+  end
+
   def voted?(user)
     lovers.include?(user) or foes.include?(user)
   end
