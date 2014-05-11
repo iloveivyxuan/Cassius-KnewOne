e43a316e5b5496e25c86cc703cb68d766928cf17
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-05-12 01:15:29 +0800

    Skip image loading

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,11 @@ require 'capybara/rspec'
 Capybara.asset_host = "http://#{Settings.host}"
 Capybara.default_wait_time = 5
 Capybara.javascript_driver = :webkit
+Capybara.register_driver :webkit do |app|
+  driver = Capybara::Webkit::Driver.new(app)
+  driver.browser.set_skip_image_loading true
+  driver
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
