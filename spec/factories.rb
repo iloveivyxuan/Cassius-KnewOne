873f77f83e7ce812c25e9b94a87956826f570b59
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-05-12 17:44:00 +0800

    Add factory of Thing

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,18 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:author] do
     sequence(:name) { |i| "#{i}-#{Faker::Name.first_name}-#{Faker::Name.last_name}" }
     email { "#{name}@example.com" }
     password 'password'
 
     before(:create) { |user| user.skip_confirmation! }
+  end
+
+  factory :thing do
+    author
+    title    { Faker::Lorem.word }
+    subtitle { Faker::Lorem.sentence }
+    content  { Faker::Lorem.paragraph }
+    categories { ["category-#{rand(3)}"] }
+
   end
 end
