aa5fd8df9ca59db0d1e6345b82fd6906fed4f364
jasl <jasl9187@hotmail.com>
2014-05-19 11:55:49 +0800

    change new relic config

--- config/initializers/new_relic.rb
+++
@@ -1,4 +0,0 @@
-if Rails.environment.production? && defined?(Unicorn) && File.basename($0).start_with?('unicorn')
-  ::NewRelic::Agent.manual_start()
-  ::NewRelic::Agent.after_fork(:force_reconnect => true)
-end
