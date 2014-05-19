0294ed011d936986c6556f905fec648027e549e8
jasl <jasl9187@hotmail.com>
2014-05-20 01:12:35 +0800

    restore genrate way bill

--- app/models/order.rb
+++ app/models/order.rb
@@ -157,9 +157,9 @@ class Order
     confirm_free! if can_confirm_free?
   end
 
-  after_save do
-    generate_waybill! if confirmed?
-  end
+  # after_save do
+  #   generate_waybill! if confirmed?
+  # end
 
   default_scope -> { order_by(created_at: :desc) }
 
@@ -417,7 +417,7 @@ class Order
 
   def generate_waybill!
     return unless waybill.url.nil?
-    # WayBillWorker.perform_async(self.id.to_s)
+    WayBillWorker.perform_async(self.id.to_s)
   end
 
   def own_things
