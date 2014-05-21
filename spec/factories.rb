ad55a79127a4ecdcbab72e9b3190281705cfd82a
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-05-21 12:07:04 +0800

    Add factory of User

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,9 @@
 FactoryGirl.define do
+  factory :user do
+    sequence(:name) { |i| "#{i}-#{Faker::Name.first_name}-#{Faker::Name.last_name}" }
+    email { "#{name}@example.com" }
+    password 'password'
+
+    before(:create) { |user| user.skip_confirmation! }
+  end
 end
