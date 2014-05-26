95107e449d992bd621c055bb57f4f78eb5202550
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-05-26 12:54:01 +0800

    Add models/order_spec

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,180 @@
+require 'spec_helper'
+
+describe Order do
+  let(:user) { create(:user, :with_cart_items, :with_addresses) }
+  let(:address) { user.addresses.first }
+  let(:items_price) { user.cart_items.map(&:price).reduce(:+) }
+
+  let(:order) { create(:order, user: user) }
+  let(:order_history) { order.order_histories.last }
+  let(:trade_no)   { Faker::Number.number(10) }
+  let(:deliver_no) { Faker::Number.number(10) }
+  let(:payment_method) { Order::PAYMENT_METHOD.keys.sample }
+  let(:note) { Faker::Lorem.sentence }
+
+  describe '.build_order' do
+    subject { Order.build_order(user) }
+
+    it { should be_an Order }
+    its(:items_price) { should eq items_price }
+    its(:should_pay_price) { should be >= subject.items_price }
+  end
+
+  context 'when use balance' do
+    let(:order) { Order.build_order(user, address_id: address.id, use_balance: true) }
+
+    context 'when balance is not enough' do
+      before do
+        user.recharge_balance!(0.1, '')
+        order.save!
+      end
+
+      specify do
+        expect(order.state).to eq :pending
+        expect(order.expense_balance).to eq 0.1
+        expect(user.balance).to eq 0
+      end
+    end
+
+    context 'when balance is enough' do
+      before do
+        user.recharge_balance!(items_price + 100, '')
+        order.save!
+      end
+
+      specify do
+        expect(order.state).to eq :confirmed
+        expect(order_history.from).to eq :freed
+        expect(order_history.to).to eq :confirmed
+        expect(user.balance).to eq 100 - order.deliver_price
+      end
+    end
+  end
+
+  describe '#confirm_payment!' do
+    let(:price) { order.should_pay_price }
+    let(:raw) { {trade_no: trade_no} }
+
+    before do
+      order.add_items_from_cart(user.cart_items)
+      order.confirm_payment!(trade_no, price, payment_method, raw)
+    end
+
+    specify do
+      expect(order.state).to eq :confirmed
+      expect(order.payment_method).to eq payment_method
+      expect(order.trade_no).to eq trade_no
+      expect(order.trade_price).to eq price
+
+      expect(order_history.from).to eq :pending
+      expect(order_history.to).to eq :confirmed
+      expect(order_history.raw).to eq raw
+
+      expect(order.user.karma).to eq Settings.karma.order
+    end
+  end
+
+  describe '#ship!' do
+    before do
+      order.state = :confirmed
+      order.ship!(deliver_no, note)
+    end
+
+    specify do
+      expect(order.state).to eq :shipped
+      expect(order.deliver_no).to eq deliver_no
+      expect(order_history.from).to eq :confirmed
+      expect(order_history.to).to eq :shipped
+    end
+  end
+
+  describe '#cancel!' do
+    before do
+      order.expense_balance = 42
+      order.cancel!
+    end
+
+    specify do
+      expect(order.state).to eq :canceled
+      expect(order_history.from).to eq :pending
+      expect(order_history.to).to eq :canceled
+
+      expect(order.expense_balance).to eq 0
+      expect(user.balance).to eq 42
+    end
+  end
+
+  describe '#close!' do
+    before do
+      order.expense_balance = 42
+      order.close!
+    end
+
+    specify do
+      expect(order.state).to eq :closed
+      expect(order_history.from).to eq :pending
+      expect(order_history.to).to eq :closed
+
+      expect(order.expense_balance).to eq 0
+      expect(user.balance).to eq 42
+    end
+  end
+
+  describe '#refund!' do
+    before do
+      order.state = :shipped
+      order.refund!
+    end
+
+    specify do
+      expect(order.state).to eq :refunded_to_platform
+      expect(order_history.from).to eq :shipped
+      expect(order_history.to).to eq :refunded_to_platform
+    end
+  end
+
+  describe '#refund_to_balance!' do
+    before do
+      order.state = :shipped
+      order.refund_to_balance!(order.total_price)
+    end
+
+    specify do
+      expect(order.state).to eq :refunded_to_balance
+      expect(order_history.from).to eq :shipped
+      expect(order_history.to).to eq :refunded_to_balance
+
+      expect(order.expense_balance).to eq 0
+      expect(user.balance).to eq order.total_price
+    end
+  end
+
+  describe '#refunded_balance_to_platform!' do
+    before do
+      order.state = :shipped
+      order.refund_to_balance!(order.total_price)
+      order.refunded_balance_to_platform!
+    end
+
+    specify do
+      expect(order.state).to eq :refunded_to_platform
+      expect(order_history.from).to eq :refunded_to_balance
+      expect(order_history.to).to eq :refunded_to_platform
+
+      expect(order.expense_balance).to eq 0
+      expect(user.balance).to eq 0
+    end
+  end
+
+  describe '#unexpect!' do
+    before do
+      order.unexpect!
+    end
+
+    specify do
+      expect(order.state).to eq :unexpected
+      expect(order_history.from).to eq :pending
+      expect(order_history.to).to eq :unexpected
+    end
+  end
+end
