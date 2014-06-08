6836e2ca7293feeeaa9adc610d2ce77aa651ee0b
jasl <jasl9187@hotmail.com>
2014-06-08 23:29:16 +0800

    add entry preview

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,5 +4,6 @@ class EntriesController < ApplicationController
 
   def show
     @entry = Entry.find(params[:id])
+    redirect_to root_path if !@entry.published? && !current_user.try(:role?, :editor)
   end
 end
