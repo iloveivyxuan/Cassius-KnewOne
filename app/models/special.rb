6f101bfb9dd65e7c7b93f51613ebfe3187309b64
jasl <jasl9187@hotmail.com>
2014-06-15 12:38:14 +0800

    add intro & ending to special

--- app/models/special.rb
+++ app/models/special.rb
@@ -1,4 +1,8 @@
 class Special < Post
   embeds_many :special_subjects
   accepts_nested_attributes_for :special_subjects, allow_destroy: true
+
+  field :intro, type: String
+  field :ending, type: String
+  field :show_nav, type: Boolean, default: true
 end
