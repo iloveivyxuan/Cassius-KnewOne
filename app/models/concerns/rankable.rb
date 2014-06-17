91932135914d072ee4c9aaaa659db987889302a2
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-06-17 19:44:22 +0800

    Add Rankable

---
+++ app/models/concerns/rankable.rb
@@ -0,0 +1,25 @@
+module Rankable
+  extend ActiveSupport::Concern
+
+  included do
+    field :heat, type: Float
+    index heat: -1
+  end
+
+  def freezing_coefficient
+    days_after_created = 1.0 * (Time.now - created_at) / 1.day
+    (days_after_created + 1) ** -1.4
+  end
+
+  def calculate_heat
+    raise 'Not implemented'
+  end
+
+  module ClassMethods
+    def update_all_heat_since(time)
+      where(:created_at.gt => time).each do |x|
+        x.update(heat: x.calculate_heat)
+      end
+    end
+  end
+end
