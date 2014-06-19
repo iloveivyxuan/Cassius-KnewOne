44eed371d07dac306f5035d190809c383eaf9d03
jasl <jasl9187@hotmail.com>
2014-06-19 16:04:38 +0800

    mailer defination

---
+++ app/mailers/thing_mailer.rb
@@ -0,0 +1,14 @@
+#encoding: utf-8
+class ThingMailer < BaseMailer
+  def encourage_owner(thing, user)
+
+  end
+
+  def encourage_review_author(user)
+
+  end
+
+  def encourage_thing_author(user)
+
+  end
+end
