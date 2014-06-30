93f365c70bfee12420abd817603a7f18708ceb37
dominic.wang <dominic@knewone.com>
2014-06-30 15:20:42 +0800

    Revert "comment douban filter"
    This reverts commit 669956de4992f39f2b82d2533fa28eac89e7cac3.

--- app/controllers/haven/users_controller.rb
+++ app/controllers/haven/users_controller.rb
@@ -161,9 +161,11 @@ module Haven
         end
         #if params[:filter].include? 'reg_douban'
          # @users = @users.where(:'auths.provider' => 'douban')
+         # logger.info("has douban in filter#{@users.size}")
         #end
         if params[:filter].include? 'reg_twitter'
           @users = @users.where(:'auths.provider' => 'twitter')
+          logger.info("has twitter in filter#{@users.size}")
         end
       else
         @users = @users.order_by([:created_at, :desc])
