0f616a69320230a538a822d0e6e97d0019aa0c6f
lilu <lilu.life@gmail.com>
2014-07-07 15:45:23 +0800

    add hits pager route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -50,7 +50,7 @@ class HomeController < ApplicationController
   end
 
   def hits
-    @batch = 4
+    @batch = 5
     @things = Thing.hot.page(params[:page]).per(6*@batch)
     @reviews = Review.hot.page(params[:page]).per(@batch)
   end
