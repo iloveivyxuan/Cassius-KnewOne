e622a1022dececfc69b6ef6135e2062baea102f3
jasl <jasl9187@hotmail.com>
2014-07-16 17:56:23 +0800

    fix uploader default_url

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@ class AvatarUploader < ImageUploader
   # Provide a default URL as a default if there hasn't been a file uploaded
   def default_url
     i = 1.upto(22).to_a.shuffle.first
-    "http://#{upyun_bucket_domain}/logos/" +
+    "#{upyun_bucket_host}/logos/" +
         ["#{i}.png", version_name].compact.join('!')
   end
 end
