c971f00cc1a774eb69c5a38e7fedbf35260defb8
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-07-21 12:24:08 +0800

    Remove sign-in spec with js

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,26 +3,8 @@ require 'spec_helper'
 feature 'Sign in' do
   given(:user) { create(:user) }
 
-  background do
-    visit root_path
-  end
-
-  scenario 'with email', js: true do
-    within '.landing .entry_email' do
-      click_link '登录'
-    end
-
-    within '#email_sign_in_pop_up' do
-      fill_in '邮箱地址', with: user.email
-      fill_in '密码',     with: user.password
-      click_button '登录'
-    end
-
-    expect(page).to have_content('登出')
-  end
-
   scenario 'with helper' do
-    user = create_signed_in_user
+    create_signed_in_user
     visit root_path
     expect(page).to have_content('登出')
   end
