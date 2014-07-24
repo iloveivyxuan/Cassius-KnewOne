8723c320d2bcffdebe6ad200df0dca900004a195
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-07-24 22:24:08 +0800

    Add controllers

---
+++ app/controllers/thing_list_items_controller.rb
@@ -0,0 +1,27 @@
+class ThingListItemsController < ApplicationController
+  load_and_authorize_resource :thing_list
+  load_and_authorize_resource :thing_list_item, through: :thing_list
+
+  respond_to :json
+
+  def create
+    @thing_list_item.save
+    respond_with @thing_list, @thing_list_item
+  end
+
+  def update
+    @thing_list_item.update(thing_list_item_params)
+    respond_with @thing_list_item
+  end
+
+  def destroy
+    @thing_list_item.destroy
+    respond_with @thing_list_item
+  end
+
+  private
+
+  def thing_list_item_params
+    params.require(:thing_list_item).permit(:thing_id, :description)
+  end
+end
---
+++ app/controllers/thing_lists_controller.rb
@@ -0,0 +1,41 @@
+class ThingListsController < ApplicationController
+  load_and_authorize_resource :user
+  load_and_authorize_resource :thing_list, through: :user, shallow: true
+
+  respond_to :html, :json
+
+  def index
+    unless @user
+      @user = current_user
+      @thing_lists = current_user.thing_lists
+    end
+
+    respond_with @thing_lists
+  end
+
+  def show
+    respond_with @thing_list
+  end
+
+  def create
+    @thing_list.user = current_user
+    @thing_list.save
+    respond_with @thing_list
+  end
+
+  def update
+    @thing_list.update(thing_list_params)
+    respond_with @thing_list
+  end
+
+  def destroy
+    @thing_list.destroy
+    respond_with @thing_list
+  end
+
+  private
+
+  def thing_list_params
+    params.require(:thing_list).permit(:name, :description)
+  end
+end
