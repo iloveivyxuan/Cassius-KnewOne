a7e49825f64a2f25d40643974411eb2dd6d5c38d
jasl <jasl9187@hotmail.com>
2014-08-01 03:20:13 +0800

    edm for weloop

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -36,6 +36,15 @@ class UserMailer < BaseMailer
          subject: "你在「KnewOne 牛玩」上喜欢的产品\"#{thing.title}\"到货了")
   end
 
+  def weloop(email, name)
+    attachments.inline['weloop.jpg'] = File.read(Rails.root.join('app/assets/images/mails/weloop.jpg'))
+    @name = name
+
+    mail(to: email,
+         subject: '仲夏夜，免费领养智能手表',
+         edm: true)
+  end
+
   def private_message(dialog)
     @dialog = dialog
     @receiver = dialog.user
