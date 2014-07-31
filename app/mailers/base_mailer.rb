f48ef2dd46561974570d7030b26a367a6c5ac444
jasl <jasl9187@hotmail.com>
2014-08-01 02:51:12 +0800

    migrate to sendcloud standalone ip

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -5,29 +5,55 @@ class BaseMailer < ActionMailer::Base
           reply_to: 'help@knewone.com'
 
   SMTP_SERVERS = {
-      white: {
-          from: 'KnewOne <welcome@mg.knewone.com>',
-          delivery_settings: {
-              address: 'smtp.mailgun.org',
-              port: 25,
-              domain: 'knewone.com',
-              user_name: 'postmaster@mg.knewone.com',
-              password: '2i7eb7yyb7k0',
-              authentication: 'plain',
-              enable_starttls_auto: true
-          }
-      },
-      default: {
-          from: 'KnewOne <welcome@service.knewone.com>',
-          delivery_settings: {
-              address: 'smtpcloud.sohu.com',
-              port: 25,
-              user_name: 'postmaster@knewone-service.sendcloud.org',
-              password: 'q4yoSPmu',
-              authentication: 'login',
-              domain: 'sendcloud.org'
-          }
+    white: {
+      from: 'KnewOne <welcome@mg.knewone.com>',
+      delivery_settings: {
+        address: 'smtp.mailgun.org',
+        port: 25,
+        domain: 'knewone.com',
+        user_name: 'postmaster@mg.knewone.com',
+        password: '2i7eb7yyb7k0',
+        authentication: 'plain',
+        enable_starttls_auto: true
       }
+    },
+    default: {
+      from: 'KnewOne <welcome@service.knewone.com>',
+      delivery_settings: {
+        address: 'smtpcloud.sohu.com',
+        port: 25,
+        user_name: 'postmaster@notify.service.knewone.com',
+        password: 'ckfsuXRzZugsoHib',
+        authentication: 'login',
+        domain: 'sendcloud.org'
+      }
+    }
+  }
+
+  EDM_SMTP_SERVERS = {
+    white: {
+      from: 'KnewOne <welcome@mg.knewone.com>',
+      delivery_settings: {
+        address: 'smtp.mailgun.org',
+        port: 25,
+        domain: 'knewone.com',
+        user_name: 'postmaster@mg.knewone.com',
+        password: '2i7eb7yyb7k0',
+        authentication: 'plain',
+        enable_starttls_auto: true
+      }
+    },
+    default: {
+      from: 'KnewOne <welcome@service.knewone.com>',
+      delivery_settings: {
+        address: 'smtpcloud.sohu.com',
+        port: 25,
+        user_name: 'postmaster@info.service.knewone.com',
+        password: 'ckfsuXRzZugsoHib',
+        authentication: 'login',
+        domain: 'sendcloud.org'
+      }
+    }
   }
 
   WHITES = %w(gmail.com ruby-china.org)
@@ -36,7 +62,7 @@ class BaseMailer < ActionMailer::Base
     attachments.inline['logo.png'] = File.read(Rails.root.join("app/assets/images/logos/#{rand(21)+1}.png"))
 
     if self.delivery_method == :smtp
-      smtp_config = route_smtp_server(headers[:to])
+      smtp_config = route_smtp_server(headers[:to], headers.delete(:edm))
       m = super headers.merge(from: smtp_config[:from]), &block
       m.delivery_method.settings.merge!(smtp_config[:delivery_settings])
       m
@@ -47,7 +73,11 @@ class BaseMailer < ActionMailer::Base
 
   private
 
-  def route_smtp_server(email)
-    SMTP_SERVERS[(WHITES.include?(/.*@(.+)/.match(email)[1]) ? :white : :default)]
+  def route_smtp_server(email, edm = nil)
+    if edm
+      EDM_SMTP_SERVERS[(WHITES.include?(/.*@(.+)/.match(email)[1]) ? :white : :default)]
+    else
+      SMTP_SERVERS[(WHITES.include?(/.*@(.+)/.match(email)[1]) ? :white : :default)]
+    end
   end
 end
