3b67408ae9052c935ac1a314fb3f565bbbcd812d
jasl <jasl9187@hotmail.com>
2014-08-05 01:41:47 +0800

    add DKIM sign for sendcloud; add sendcloud_fallback for qq.com; refactor deliver router by interceptor

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -3,81 +3,11 @@ class BaseMailer < ActionMailer::Base
   layout 'mailer'
   default content_type: 'text/html',
           reply_to: 'help@knewone.com'
+  before_action :set_logo
 
-  SMTP_SERVERS = {
-    white: {
-      from: 'KnewOne <welcome@mg.knewone.com>',
-      delivery_settings: {
-        address: 'smtp.mailgun.org',
-        port: 25,
-        domain: 'knewone.com',
-        user_name: 'postmaster@mg.knewone.com',
-        password: '2i7eb7yyb7k0',
-        authentication: 'plain',
-        enable_starttls_auto: true
-      }
-    },
-    default: {
-      from: 'KnewOne <welcome@service.knewone.com>',
-      delivery_settings: {
-        address: 'smtpcloud.sohu.com',
-        port: 25,
-        user_name: 'postmaster@notify.service.knewone.com',
-        password: 'ckfsuXRzZugsoHib',
-        authentication: 'login',
-        domain: 'sendcloud.org'
-      }
-    }
-  }
+  protected
 
-  EDM_SMTP_SERVERS = {
-    white: {
-      from: 'KnewOne <welcome@mg.knewone.com>',
-      delivery_settings: {
-        address: 'smtp.mailgun.org',
-        port: 25,
-        domain: 'knewone.com',
-        user_name: 'postmaster@mg.knewone.com',
-        password: '2i7eb7yyb7k0',
-        authentication: 'plain',
-        enable_starttls_auto: true
-      }
-    },
-    default: {
-      from: 'KnewOne <welcome@service.knewone.com>',
-      delivery_settings: {
-        address: 'smtpcloud.sohu.com',
-        port: 25,
-        user_name: 'postmaster@info.service.knewone.com',
-        password: 'ckfsuXRzZugsoHib',
-        authentication: 'login',
-        domain: 'sendcloud.org'
-      }
-    }
-  }
-
-  WHITES = %w(gmail.com ruby-china.org)
-
-  def mail(headers = {}, &block)
+  def set_logo
     attachments.inline['logo.png'] = File.read(Rails.root.join("app/assets/images/logos/#{rand(21)+1}.png"))
-
-    if self.delivery_method == :smtp
-      smtp_config = route_smtp_server(headers[:to], headers.delete(:edm))
-      m = super headers.merge(from: smtp_config[:from]), &block
-      m.delivery_method.settings.merge!(smtp_config[:delivery_settings])
-      m
-    else
-      super headers, &block
-    end
-  end
-
-  private
-
-  def route_smtp_server(email, edm = nil)
-    if edm
-      EDM_SMTP_SERVERS[(WHITES.include?(/.*@(.+)/.match(email)[1]) ? :white : :default)]
-    else
-      SMTP_SERVERS[(WHITES.include?(/.*@(.+)/.match(email)[1]) ? :white : :default)]
-    end
   end
 end
