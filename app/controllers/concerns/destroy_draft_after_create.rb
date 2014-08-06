b650652ef272a3b7fde74c918dfca4515a00db18
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-08-06 17:42:23 +0800

    Don't destroy draft when status code >= 400

--- app/controllers/concerns/destroy_draft_after_create.rb
+++ app/controllers/concerns/destroy_draft_after_create.rb
@@ -9,6 +9,7 @@ module DestroyDraftAfterCreate
 
   def destroy_draft
     return unless user_signed_in? && params[:draft].try(:[], :key)
+    return if response.status >= 400
     current_user.drafts.where(key: params[:draft][:key]).destroy
   end
 end
