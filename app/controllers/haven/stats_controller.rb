d1cf46493d769c681115fac2b0ce0c2f2948ccfb
liamzhao <sethverlo5@gmail.com>
2014-08-08 11:02:25 +0800

    typo

--- app/controllers/haven/stats_controller.rb
+++ app/controllers/haven/stats_controller.rb
@@ -5,7 +5,7 @@ module Haven
     def index
       if params["date"]
         @date_from = Date.new(params["date"]["from(1i)"].to_i, params["date"]["from(2i)"].to_i, params["date"]["from(3i)"].to_i)
-        @date_to = date = Date.new(params["date"]["to(1i)"].to_i, params["date"]["to(2i)"].to_i, params["date"]["to(3i)"].to_i)
+        @date_to = Date.new(params["date"]["to(1i)"].to_i, params["date"]["to(2i)"].to_i, params["date"]["to(3i)"].to_i)
         @new_users = User.where(:created_at.gte => @date_from).where(:created_at.lte => @date_to.next_day).all.size
         @new_things = Activity.by_type(:new_thing).from_date(@date_from).to_date(@date_to).all.size
         @new_reviews = Activity.by_type(:new_review).from_date(@date_from).to_date(@date_to).all.size
