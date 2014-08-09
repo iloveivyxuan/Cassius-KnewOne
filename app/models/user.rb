b8acea386f7e25467bd4f88b939b2cde86e75413
jasl <jasl9187@hotmail.com>
2014-08-10 02:50:49 +0800

    some user feedback they wont to receive edm mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,9 @@ class User
 
   index name: 1
 
+  field :accept_edm, type: Boolean, default: true
+  scope :edm, -> { where accept_edm: true }
+
   STATUS = {blocked: '锁定', watching: '特别观照(贬)', normal: '正常'}
   validates :status, inclusion: {in: STATUS.keys, allow_blank: false}
   validates :gender, inclusion: {in: %w(男 女), allow_blank: true}
