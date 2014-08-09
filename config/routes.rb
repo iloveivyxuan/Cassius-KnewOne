02cf2597037fcd9e5eb25f03c9673de9d47be877
lilu <lilu.life@gmail.com>
2014-08-09 12:13:02 +0800

    shop routes url

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Making::Application.routes.draw do
   get "500", to: "home#error"
   get 'blocked', to: 'home#blocked'
 
-  get 'shop/(:page)', to: 'things#shop', as: :shop
+  get 'shop/(:order_by)/(:page)', to: 'things#shop', as: :shop
 
   get 'help', to: 'help#index'
   %w(how_to_share how_to_review terms knewone_for_user knewone_for_startup).each do |a|
