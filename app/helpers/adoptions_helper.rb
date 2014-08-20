f4240ddbadb04d849afb22ed9306ab7c17c6e50e
liamzhao <sethverlo5@gmail.com>
2014-08-20 19:02:04 +0800

    adoption helper

---
+++ app/helpers/adoptions_helper.rb
@@ -0,0 +1,16 @@
+module AdoptionsHelper
+  def adoption_status(status)
+    case status
+    when :waiting
+      "领养进行中"
+    when :denied
+      "领养失败"
+    when :approved
+      "领养成功"
+    end
+  end
+
+  def adopted_by(thing, user)
+    @_is_adopted ||= Adoption.where(thing: thing, user: user).exists?
+  end
+end
