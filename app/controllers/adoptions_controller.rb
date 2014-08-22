652380b154d1bbb6848672fe7c32305803a13de1
lilu <lilu.life@gmail.com>
2014-08-22 17:44:03 +0800

    remove kinds and adoption_no from adoption

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -2,45 +2,26 @@ class AdoptionsController < ApplicationController
   before_action :require_signed_in
   layout 'settings'
 
-  def new
-    @thing = Thing.find params[:id]
-    @adoptions = @thing.adoptions
-    @adoption_users_size = @adoptions.map(&:user).uniq.size
-  end
-
   def index
     @adoptions = current_user.adoptions
   end
 
   def create
-    if params[:adoption][:address_id] == 'new'
-      address = current_user.addresses.build(adoption_params[:address])
-      if address.save
-        params[:adoption][:address_id] = address.id.to_s
-      else
-        current_user.addresses.delete(address)
-        @adoption = Adoption.build_adoption(current_user, nil)
-        render 'new'
-        return
-      end
-    end
+    address = if params[:adoption][:address_id] == 'new'
+                current_user.addresses.create! adoption_params[:address]
+              else
+                current_user.addresses.find params[:adoption][:address_id]
+              end
 
-    @adoption = Adoption.build_adoption(current_user, adoption_params)
-    @adoption.address = current_user.addresses.find_by(id: params[:adoption][:address_id])
-    @adoption.adoption_no = rand.to_s[2..11]
-    @adoption.kind = @adoption.thing.kinds.first if @adoption.kind.empty?
-    unless Adoption.where(thing: @adoption.thing, user: current_user).exists?
-      @adoption.save!
-    end
-  end
+    params[:adoption].delete :address
+    params[:adoption].delete :address_id
 
-  def show
-    @adoption = Adoption.find params[:id]
+    @adoption = Adoption.create! adoption_params.merge(user: current_user, address: address)
   end
 
   private
 
   def adoption_params
-    params.require(:adoption).permit(:kind, :note, :thing, :address_id, address: [:province, :district, :street, :name, :phone, :zip_code, :default])
+    params.require(:adoption).permit(:note, :thing, :address_id, address: [:province, :district, :street, :name, :phone, :zip_code, :default])
   end
 end
--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -44,20 +44,7 @@ module ThingsHelper
         }
       end
     when :adoption
-      if user_signed_in?
-        link_to_with_icon "领养", "fa fa-shopping-cart fa-lg", "#",
-        title: thing.title,
-        class: "btn btn-success btn-block track_event",
-        data: {
-          action: "buy",
-          category: "adoption",
-          label: "buy+adoption+#{thing.title}",
-          target: "#adoption-modal",
-          toggle: "modal"
-        }
-      else
-        link_to_with_icon "请登录后领养", "fa fa-sign-in", "#", class: "btn btn-danger btn-block", data: {toggle: "modal", target: "#login-modal"}
-      end
+      render 'things/adopt', tp: present(thing)
     else
       nil
     end
