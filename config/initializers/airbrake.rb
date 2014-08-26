39dec415a526b8e814fefb30a4e00e1b501bbd53
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-08-26 11:25:45 +0800

    Fix Airbrake config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,3 @@
-if Rails.env.production?
-  Airbrake.configure do |config|
-    config.api_key = 'c7ac118b281cc0cb99161502acb712e3'
-  end
+Airbrake.configure do |config|
+  config.api_key = 'c7ac118b281cc0cb99161502acb712e3'
 end
