374080fa7c58a23c2a1f16e6a1e64f6c4761cb52
liamzhao <sethverlo5@gmail.com>
2014-09-02 16:39:33 +0800

    add login tracker

--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -3,39 +3,43 @@ module ThingsHelper
     case thing.stage
     when :dsell
       if user_signed_in?
-        link_to_with_icon "购买", "fa fa-shopping-cart", "#", class: "btn btn-success btn-block", data: {toggle: "modal", target: "#mobile_buy_modal"}
+        link_to_with_icon "购买", "fa fa-shopping-cart", "#",
+        class: "btn btn-success btn-block track_event",
+        data: data_with_buy_tracker("dsell", thing.title, {toggle: "modal", target: "#mobile_buy_modal"})
       else
-        link_to_with_icon "请登录后购买", "fa fa-sign-in", "#", class: "btn btn-danger btn-block", data: {toggle: "modal", target: "#login-modal"}
+        link_to_with_icon "请登录后购买", "fa fa-sign-in", "#",
+        class: "btn btn-danger btn-block track_event",
+        data: data_with_login_tracker("dsell", thing.title)
       end
     when :pre_order
       if user_signed_in?
-        link_to_with_icon "购买", "fa fa-shopping-cart", "#", class: "btn btn-success btn-block", data: {toggle: "modal", target: "#mobile_buy_modal"}
+        link_to_with_icon "购买", "fa fa-shopping-cart", "#",
+        class: "btn btn-success btn-block track_event",
+        data: data_with_buy_tracker("pre_order", thing.title, {toggle: "modal", target: "#mobile_buy_modal"})
       else
-        link_to_with_icon "请登录后购买", "fa fa-sign-in", "#", class: "btn btn-danger btn-block", data: {toggle: "modal", target: "#login-modal"}
+        link_to_with_icon "请登录后购买", "fa fa-sign-in", "#",
+        class: "btn btn-danger btn-block track_event",
+        data: data_with_login_tracker("pre_order", thing.title)
       end
     when :kick
       if user_signed_in? && thing.shop.present?
         link_to_with_icon "众筹", "fa fa-fire fa-lg", buy_thing_path(thing),
         title: thing.title, class: "btn btn-warning btn-block buy_button track_event", target: "_blank", rel: '_nofollow',
-        data: {
-          action: "buy",
-          category: "kick",
-          label: "buy+kick+#{thing.title}"
-        }
+        data: data_with_buy_tracker("kick", thing.title)
       else
-        link_to_with_icon "请登录后众筹", "fa fa-sign-in", "#", class: "btn btn-danger btn-block", data: {toggle: "modal", target: "#login-modal"}
+        link_to_with_icon "请登录后众筹", "fa fa-sign-in", "#",
+        class: "btn btn-danger btn-block track_event",
+        data: data_with_login_tracker("kick", thing.title)
       end
     when :domestic
       if user_signed_in? && thing.shop.present?
         link_to_with_icon "网购", "fa fa-location-arrow fa-lg", buy_thing_path(thing),
         title: thing.title, class: "btn btn-info btn-block buy_button track_event", target: "_blank", rel: '_nofollow',
-        data: {
-          action: "buy",
-          category: "domestic",
-          label: "buy+domestic+#{thing.title}"
-        }
+        data: data_with_buy_tracker("domestic", thing.title)
       else
-        link_to_with_icon "请登录后网购", "fa fa-sign-in", "#", class: "btn btn-danger btn-block", data: {toggle: "modal", target: "#login-modal"}
+        link_to_with_icon "请登录后网购", "fa fa-sign-in", "#",
+        class: "btn btn-danger btn-block track_event",
+        data: data_with_login_tracker("domestic", thing.title)
       end
     when :abroad
       if user_signed_in? && thing.shop.present?
@@ -47,13 +51,36 @@ module ThingsHelper
           label: "buy+abroad+#{thing.title}"
         }
       else
-        link_to_with_icon "请登录后海淘", "fa fa-sign-in", "#", class: "btn btn-danger btn-block", data: {toggle: "modal", target: "#login-modal"}
+        link_to_with_icon "请登录后海淘", "fa fa-sign-in", "#",
+        class: "btn btn-danger btn-block track_event",
+        data: data_with_login_tracker("abroad", thing.title)
       end
     when :adoption
       render 'things/adopt', tp: present(thing)
     else
       nil
     end
+  end
+
+  def data_with_buy_tracker(category, label, data=nil)
+    tracker = {
+      action: "buy",
+      category: "buy+#{category}",
+      label: "buy+#{category}+#{label}"
+    }
+
+    data ? tracker.merge(data) : tracker
+  end
+
+  def data_with_login_tracker(category, label)
+    {
+      action: "login",
+      category: "login+#{category}",
+      label: "login+#{category}+#{label}",
+
+      toggle: "modal",
+      target: "#login-modal"
+    }
   end
 
 end
--- app/presenters/thing_presenter.rb
+++ app/presenters/thing_presenter.rb
@@ -69,15 +69,13 @@ class ThingPresenter < PostPresenter
     return concept unless thing.shop.present?
 
     if user_signed_in?
-      link_to_with_icon "网购", "fa fa-location-arrow fa-lg", buy_thing_path(thing),
-      title: title, class: "btn btn-info btn-block buy_button track_event", target: "_blank", rel: '_nofollow',
-      data: {
-        action: "buy",
-        category: "domestic",
-        label: "buy+domestic+#{title}"
-      }
+      link_to_with_icon "网购", "fa fa-location-arrow fa-lg", buy_thing_path(thing), title: title,
+      class: "btn btn-info btn-block buy_button track_event", target: "_blank", rel: '_nofollow',
+      data: data_with_buy_tracker("domestic", thing.title)
     else
-      link_to_with_icon "请登录后网购", "fa fa-sign-in", "#", class: "btn btn-info btn-block buy_button", data: {toggle: "modal", target: "#login-modal"}
+      link_to_with_icon "请登录后网购", "fa fa-sign-in", "#",
+      class: "btn btn-info btn-block buy_button track_event",
+      data: data_with_login_tracker("domestic", thing.title)
     end
   end
 
@@ -87,13 +85,11 @@ class ThingPresenter < PostPresenter
     if user_signed_in?
       link_to_with_icon "众筹", "fa fa-fire fa-lg", buy_thing_path(thing),
       title: title, class: "btn btn-warning btn-block buy_button track_event", target: "_blank", rel: '_nofollow',
-      data: {
-        action: "buy",
-        category: "kick",
-        label: "buy+kick+#{title}"
-      }
+      data: data_with_buy_tracker("kick", thing.title)
     else
-      link_to_with_icon "请登录后众筹", "fa fa-sign-in", "#", class: "btn btn-warning btn-block buy_button", data: {toggle: "modal", target: "#login-modal"}
+      link_to_with_icon "请登录后众筹", "fa fa-sign-in", "#",
+      class: "btn btn-warning btn-block buy_button track_event",
+      data: data_with_login_tracker("kick", thing.title)
     end
   end
 
@@ -103,13 +99,11 @@ class ThingPresenter < PostPresenter
     if user_signed_in?
       link_to_with_icon "海淘", "fa fa-plane fa-lg", buy_thing_path(thing),
       title: title, class: "btn btn-info btn-block buy_button track_event", target: "_blank", rel: '_nofollow',
-      data: {
-        action: "buy",
-        category: "abroad",
-        label: "buy+abroad+#{title}"
-      }
+      data: data_with_buy_tracker("abroad", thing.title)
     else
-      link_to_with_icon "请登录后海淘", "fa fa-sign-in", "#", class: "btn btn-info btn-block buy_button", data: {toggle: "modal", target: "#login-modal"}
+      link_to_with_icon "请登录后海淘", "fa fa-sign-in", "#",
+      class: "btn btn-info btn-block buy_button track_event",
+      data: data_with_login_tracker("abroad", thing.title)
     end
   end
 
