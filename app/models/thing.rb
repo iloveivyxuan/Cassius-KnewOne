165d6203c89d732b5a1b641ea58e35c98cda99f4
liamzhao <sethverlo5@gmail.com>
2014-09-02 18:05:16 +0800

    add new taiwan dollar

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -29,7 +29,7 @@ class Thing < Post
   field :price_unit, type: String, default: "¥"
   field :shopping_desc, type: String, default: ""
   field :period, type: DateTime
-  CURRENCY_LIST = %w{¥ $ € £ JPY¥ ₩}
+  CURRENCY_LIST = %w{¥ $ € £ JPY¥ ₩ NT$}
 
   field :priority, type: Integer, default: 0
 
