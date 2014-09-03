18fab399512d4e2bda0902afb09cd0f90470eb1d
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-09-03 15:31:40 +0800

    Turn cache off in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Making::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
   config.action_controller.action_on_unpermitted_parameters = :raise
 
   # Don't care if the mailer can't send
