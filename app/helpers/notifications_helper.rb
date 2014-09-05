b015dc9e4c898d84171aad94a59af7c42cd0c3c0
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-09-05 16:10:27 +0800

    Add anchor option to notification_post

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -17,45 +17,45 @@ module NotificationsHelper
     raw "#{links} "
   end
 
-  def notification_post(post, target = '_blank')
+  def notification_post(post, target = '_blank', anchor = nil)
     content = ""
     case post.class
     when Article then
       if entry = post.entry
         content += "文章 "
-        content += link_to post.title, entry_path(entry), target: target
+        content += link_to post.title, entry_path(entry, anchor: anchor), target: target
       else
         content += ' 失效的资源'
       end
     when Special then
       if entry = post.entry
         content += "专题 "
-        content += link_to post.title, entry_path(entry), target: target
+        content += link_to post.title, entry_path(entry, anchor: anchor), target: target
       else
         content += ' 失效的资源'
       end
     when Thing then
       content += "产品 "
-      content += link_to post.title, thing_path(post), target: target
+      content += link_to post.title, thing_path(post, anchor: anchor), target: target
     when Review then
       if post.thing
         content += "评测 "
-        content += link_to post.title, thing_review_path(post.thing, post), target: target
+        content += link_to post.title, thing_review_path(post.thing, post, anchor: anchor), target: target
       else
         content += ' 失效的资源'
       end
     when Feeling then
       if post.thing
-        content += link_to ' 短评', thing_feeling_path(post.thing, post), target: target
+        content += link_to ' 短评', thing_feeling_path(post.thing, post, anchor: anchor), target: target
       else
         content += ' 失效的资源'
       end
     when Topic then
       content += "帖子 "
-      content += link_to post.title, group_topic_path(post.group, post), target: target
+      content += link_to post.title, group_topic_path(post.group, post, anchor: anchor), target: target
     when Story then
       content += "动态 "
-      content += link_to post.title, thing_story_path(post.thing, post), target: target
+      content += link_to post.title, thing_story_path(post.thing, post, anchor: anchor), target: target
     else
       content += ' 失效的资源'
     end
