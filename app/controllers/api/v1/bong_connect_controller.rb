21411df4a3bcc23274e78e8863d742a071c36714
jasl <jasl9187@hotmail.com>
2014-09-16 17:21:28 +0800

    refactor bong_connect

--- app/controllers/api/v1/bong_connect_controller.rb
+++ app/controllers/api/v1/bong_connect_controller.rb
@@ -3,7 +3,7 @@ require 'digest/md5'
 module Api
   module V1
     class BongConnectController < ApiController
-      before_action :check_fields, :check_expired, :check_sign
+      before_action :check_fields, :check_expired, :check_sign, :set_user
 
       skip_after_action :respond_request
 
@@ -11,26 +11,21 @@ module Api
       SECRET = 'f52105d6'
 
       def create
-        user = User.where(email: params[:email]).first
-        unless user
-          user = User.create name: "#{params[:name]}@bong", password: params[:password], email: params[:email]
-        end
-
-        unless user.auths.where(provider: 'bong').exists?
-          user.auths.create uid: params[:uid], provider: 'bong', name: params[:name]
+        unless @user.auths.where(provider: 'bong').exists?
+          @user.auths.create uid: params[:uid], provider: 'bong', name: params[:name]
         end
 
         app = Doorkeeper::Application.find('5412c5bb31302d6308070000')
 
-        token = app.access_tokens.where(resource_owner_id: user.id).first
+        token = app.access_tokens.where(resource_owner_id: @user.id).first
         unless token
-          token = app.access_tokens.create! resource_owner_id: user.id,
+          token = app.access_tokens.create! resource_owner_id: @user.id,
                                             scopes: 'public',
                                             expires_in: Doorkeeper.configuration.access_token_expires_in,
                                             use_refresh_token: Doorkeeper.configuration.refresh_token_enabled?
         end
 
-        render :json => {:access_token => token.token, :user_id => user.id.to_s}
+        render :json => {:access_token => token.token, :uid => @user.id.to_s}
       end
 
       private
@@ -50,6 +45,17 @@ module Api
       def check_sign
         if params[:sign].blank? || !valid_sign?(params.except(*request.path_parameters.keys))
           render :status => :bad_request, :json => {:message => 'invalid sign'}
+        end
+      end
+
+      def set_user
+        @user = User.where(email: params[:email]).first
+        if @user
+          unless @user.valid_password?(params[:password])
+            render :status => :unauthorized, :json => {:message => 'invalid password'}
+          end
+        else
+          @user = User.create name: "#{params[:name]}@bong", password: params[:password], email: params[:email]
         end
       end
 
