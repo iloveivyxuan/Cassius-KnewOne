9d8c02515ebf6f843195f703ccb49ae7ed57ec69
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-09-17 18:49:43 +0800

    Enable Airbrake only in production and development environment

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,5 @@
-Airbrake.configure do |config|
-  config.api_key = 'c7ac118b281cc0cb99161502acb712e3'
+if Rails.env.production? || Rails.env.development?
+  Airbrake.configure do |config|
+    config.api_key = 'c7ac118b281cc0cb99161502acb712e3'
+  end
 end
