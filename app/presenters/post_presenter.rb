7894cad68245e71209a00f46262768042652e96f
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-09-19 16:59:19 +0800

    Adapt voting button

--- app/presenters/post_presenter.rb
+++ app/presenters/post_presenter.rb
@@ -70,7 +70,7 @@ class PostPresenter < ApplicationPresenter
   end
 
   def lovers_count
-    i = content_tag :i, "", class: "fa fa-plus"
+    i = content_tag :i, "", class: "fa fa-like"
     count = content_tag :span, @object.lovers_count
     i.concat count
   end
