b9a072258148ac6f688594f20743fed0714ff57e
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-09-19 19:14:55 +0800

    Fix test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,6 +6,6 @@ feature 'Sign in' do
   scenario 'with helper' do
     create_signed_in_user
     visit root_path
-    expect(page).to have_content('登出')
+    expect(page).to have_content('安全退出')
   end
 end
