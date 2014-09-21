fc59986e6179857a36c003ee2b6da1e18cd5680d
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-09-21 18:23:36 +0800

    No more truncation of ThingListItem#description

--- app/models/thing_list_item.rb
+++ app/models/thing_list_item.rb
@@ -18,10 +18,6 @@ class ThingListItem
     self.order = self.list.items.max(:order).to_i + 1
   end
 
-  before_save do
-    self.description = self.description.truncate(70)
-  end
-
   after_create do
     self.thing.fancy(list.author)
   end
