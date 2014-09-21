769c43e612b21d3767e181973bbe404583e5ed30
jasl <jasl9187@hotmail.com>
2014-09-21 17:09:49 +0800

    add high resolution avatars

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,9 +1,9 @@
 class AvatarUploader < ImageUploader
+  AVATARS = %w(1 3) + (12..20).to_a.map(&:to_s)
 
   # Provide a default URL as a default if there hasn't been a file uploaded
   def default_url
-    i = 1.upto(22).to_a.shuffle.first
-    "#{upyun_bucket_host}/logos/" +
-        ["#{i}.png", version_name].compact.join('!')
+    "#{upyun_bucket_host}/avatars/" +
+        ["#{AVATARS.sample}.png", version_name].compact.join('!')
   end
 end
