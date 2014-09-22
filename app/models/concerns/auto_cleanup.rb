20dbfd8cb03a48c34f2171caf83bc767b88ee068
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-09-22 17:54:52 +0800

    Refactor with Activity.by_reference & Activity.by_source

--- app/models/concerns/auto_cleanup.rb
+++ app/models/concerns/auto_cleanup.rb
@@ -9,9 +9,8 @@ module AutoCleanup
   private
 
   def cleanup_relevant_activities
-    union = "#{self.class.to_s}_#{id.to_s}"
-    Activity.where(reference_union: union).update_all(visible: false)
-    Activity.where(source_union: union).update_all(visible: false)
+    Activity.by_reference(self).update_all(visible: false)
+    Activity.by_source(self).update_all(visible: false)
   end
 
   def cleanup_relevant_notifications
