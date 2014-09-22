20592cc129c8fc4ca950674b1acd230ffb52d971
liamzhao <sethverlo5@gmail.com>
2014-09-22 16:24:01 +0800

    notify newer sharers after things linked

--- app/controllers/haven/links_controller.rb
+++ app/controllers/haven/links_controller.rb
@@ -28,6 +28,8 @@ module Haven
         t.save!
       end
 
+      notify_sharers_of(things)
+
       redirect_to haven_links_path
     end
 
@@ -43,6 +45,25 @@ module Haven
       Thing.find(params[:id]).delete_links
       redirect_to haven_links_path
     end
+
+    private
+
+    def notify_sharers_of(things)
+      newer_things = things.sort_by(&:created_at).reverse.take(things.count - 1)
+      oldest_thing = things.sort_by(&:created_at).take(1).first
+      knewone = User.where(id: '511114fa7373c2e3180000b4').first
+      newer_things.each do |t|
+        content = <<STR
+您好，感谢您来到 KnewOne。
+由于您分享的产品 <a href='#{thing_url(t)}'>#{t.title}</a> 之前已经被 <a href='#{user_url(oldest_thing.author)}'>#{oldest_thing.author.name}</a> 于 <a href='#{thing_url(oldest_thing)}'>#{oldest_thing.title}</a> 分享过，所以您的产品条目将被合并。您分享的产品页面将会保留，但会发生如下变化：相同产品的喜欢数、拥有数、短评和测评都将统一合并。
+
+分享产品的帮助请点击 <a href="http://knewone.com/about">KnewOne 牛玩上手指南</a>
+非常感谢您对 KnewOne 的关注，希望您能分享更多新奇酷的产品和体验，谢谢。
+STR
+        knewone.send_private_message_to(t.author, content)
+      end
+
+    end # notify_sharers_of
 
   end
 end
