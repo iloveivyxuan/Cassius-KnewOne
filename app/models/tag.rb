ddff678c9ac0790fbd5a17244cc9ea62c22b5768
liamzhao <sethverlo5@gmail.com>
2014-09-26 13:58:21 +0800

    add things_count for Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -11,9 +11,16 @@ class Tag
 
   has_and_belongs_to_many :things
 
+  field :things_count, type: Integer, default: 0
+  before_save :update_count
+
   def self.find_by_sequence(name)
     return all if name.blank?
     str = Regexp.escape(name.gsub(/[^\u4e00-\u9fa5a-zA-Z0-9_-]+/, ''))
     where(name: /^#{str}/i)
+  end
+
+  def update_count
+    self.things_count = self.things.size
   end
 end
