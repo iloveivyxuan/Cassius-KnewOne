f7a70f4ddbfc438e5dacfe1089d52d09411b0d83
孙正安 <sun-zheng-an@sza-mac.local>
2014-09-26 23:35:11 +0800

    Change User#name length range to 1..20

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User
   validates :name, presence: true, uniqueness: {case_sensitive: false},
             format: {with: /\A[^\s]+\z/, multiline: false, message: '不能包含空格'}
 
-  validates :name, length: {in: 4..20}, if: -> { self.auths.empty? }
+  validates :name, length: {in: 1..20}, if: -> { self.auths.empty? }
 
   RESERVED_WORDS = ['knewone', '知新创想', '牛玩']
   validate :name_cannot_include_reserved_words
