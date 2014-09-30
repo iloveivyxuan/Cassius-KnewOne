c7f0ee3e17774815b03c54cc17fa0236ba8a0493
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-09-30 15:18:07 +0800

    Revert "Add counter cache about ThingList size"
    This reverts commit 0d7e61300d4698338426f1aa95bb605087bd175c.

--- app/models/thing_list.rb
+++ app/models/thing_list.rb
@@ -5,7 +5,7 @@ class ThingList
 
   belongs_to :author, class_name: 'User', inverse_of: :thing_lists, index: true
   embeds_many :thing_list_items
-  field :size, type: Integer, default: 0
+  has_many :comments
 
   index 'thing_list_items.thing_id' => 1
 
