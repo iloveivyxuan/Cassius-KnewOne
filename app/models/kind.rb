6ae41104a1f29729ff7661f52544d1c83fa0d8a4
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-10-07 19:02:32 +0800

    Validate Kind#sold, fix #898

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -29,6 +29,11 @@ class Kind
       greater_than_or_equal_to: 0
   }
 
+  validates :sold, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0
+  }
+
   validates :price, numericality: {
       greater_than_or_equal_to: 0
   }
