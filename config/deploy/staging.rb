eb14fc2478184b57f06a625eb72a1aa0f94c7906
jasl <jasl9187@hotmail.com>
2014-10-14 03:08:27 +0800

    update staging deployment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,6 +4,8 @@ set :branch, 'staging'
 
 set :rails_env, 'staging'
 
+set :ssh_options, { port: 22222, forward_agent: true }
+
 set :unicorn_config_path, File.join(current_path, "config", "unicorn", "#{fetch(:rails_env)}.rb")
 
 namespace :deploy do
