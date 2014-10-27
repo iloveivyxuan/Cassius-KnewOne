289680dd11e8057e1c9d4b7abbee303337d63e18
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-10-27 14:01:25 +0800

    Add background into ThingList

--- app/models/thing_list.rb
+++ app/models/thing_list.rb
@@ -6,6 +6,7 @@ class ThingList
   belongs_to :author, class_name: 'User', inverse_of: :thing_lists, index: true
   embeds_many :thing_list_items
   has_many :comments
+  belongs_to :background, class_name: 'ThingListBackground'
 
   index 'thing_list_items.thing_id' => 1
 
