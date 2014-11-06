1e32f730b8ba35ab1502506b1f59ca4257a7098e
Syn Zhang <zeyanzhang.china@gmail.com>
2014-11-06 13:29:28 +0800

    change words in cart

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -8,7 +8,7 @@ module CartsHelper
     return '' if total_price == 0
 
     if (gap = 500 - total_price) > 0
-      "*总价已含运费(中通快递)，再选购 <strong class=\"deliver_free_gap\">#{gap}</strong> 元的产品即可免费升级至顺丰速运(或补10元)。（bong等少量特殊商品运费另算）".html_safe
+      "*总价已含运费(普通快递)，再选购 <strong class=\"deliver_free_gap\">#{gap}</strong> 元的产品即可免费升级至顺丰速运(或补10元)。（bong等少量特殊商品运费另算）".html_safe
     else
       '*总价已含运费(顺丰速运)。（bong等少量特殊商品运费另算）'
     end
