3053fc2de7ea61b015155e339f3e180e7f2ebbb1
jasl <jasl9187@hotmail.com>
2014-11-06 17:43:34 +0800

    improve wechat sharing

--- app/assets/javascripts/init_wechat.js
+++ app/assets/javascripts/init_wechat.js
@@ -1,24 +1,30 @@
 var data = {
     app: '',
-    img: function() {
+    img: function () {
         var url = $('meta[name="wechat:cover_url"]').attr('content');
-        if(url == '') {
+        if (url == '') {
             url = 'http://image.knewone.com/photos/87142c54ec6cef7d1ba8576671dd6826.jpg!small';
         }
         return url;
     },
-    desc: $('meta[name="wechat:desc"]').attr('content'),
+    desc: function () {
+        var desc = $('meta[name="wechat:desc"]').attr('content');
+        if (desc == '') {
+            desc = 'KnewOne探索新奇酷';
+        }
+        return desc;
+    },
     title: function () {
         var title = $('meta[name="wechat:title"]').attr('content');
-        if(title == '') {
-            title = $('title').text();
+        if (title == '') {
+            title = $('title').text().split(' - KnewOne')[0];
         }
         return title;
     },
     link: window.location.href
 };
 
-var callback = function(res) {
+var callback = function (res) {
     $('#share--wechat-tip').fadeOut('fast');
     // 返回的是json，包含用户是否分享还是取消
 };
