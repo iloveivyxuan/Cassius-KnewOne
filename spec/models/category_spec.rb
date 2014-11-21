48d8a6e089950de7c49031995ec8b6313c1d484f
liamzhao <sethverlo5@gmail.com>
2014-11-21 15:16:38 +0800

    add test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,27 +1,19 @@
 require 'spec_helper'
 
 describe Category, type: :model do
+  let(:thing) { create(:thing) }
+  let(:category) { create(:category) }
+  let(:inner_c) { create(:category) }
+  let(:tag) { create(:tag) }
 
   describe 'share same things between categories & tags' do
-    let(:thing) { create(:thing) }
-    let(:category) { create(:category) }
-    let(:inner_c) { create(:category) }
-    let(:tag) { create(:tag) }
-
     before do
       category.inner_categories << inner_c
       inner_c.tags << tag
       thing.tags_text = tag.name
 
-      # refresh
-      Category.update_thing_ids
-      Category.update_things_count
-      Tag.update_things_count
-
-      # reload
-      category.reload
-      inner_c.reload
-      tag.reload
+      refresh
+      reload
     end
 
     specify do
@@ -37,4 +29,33 @@ describe Category, type: :model do
     end
   end
 
+  describe 'share tags between primary & inner categories' do
+    before do
+      category.inner_categories << inner_c
+      inner_c.tags << tag
+      thing.tags_text = tag.name
+
+      refresh
+      reload
+    end
+
+    specify do
+      expect(category.tags).to include tag
+      expect(inner_c.tags).to include tag
+    end
+  end
+
+end
+
+def refresh
+  Category.update_thing_ids
+  Category.update_things_count
+  Category.update_tags
+  Tag.update_things_count
+end
+
+def reload
+  category.reload
+  inner_c.reload
+  tag.reload
 end
