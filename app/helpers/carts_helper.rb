a4fa07de1c392a8ac2105c376a6475814caadd67
Syn Zhang <zeyanzhang.china@gmail.com>
2014-12-04 12:19:24 +0800

    change words for saling

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -6,11 +6,6 @@ module CartsHelper
 
   def deliver_tip_text(total_price)
     return '' if total_price == 0
-
-    if (gap = 500 - total_price) > 0
-      "*总价已含运费(普通快递)，再选购 <strong class=\"deliver_free_gap\">#{gap}</strong> 元的产品即可免费升级至顺丰速运(或补10元)。（bong等少量特殊商品运费另算）".html_safe
-    else
-      '*总价已含运费。（bong等少量特殊商品运费另算）'
-    end
+    "*总价已含运费(普通快递)，bong 系列产品需要收取额外运费，统一发圆通快递。"
   end
 end
