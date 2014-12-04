6581c97d1281b7b6c4a8c3512da73ae410370c6f
Syn Zhang <zeyanzhang.china@gmail.com>
2014-12-04 12:44:34 +0800

    change words of deliver tip

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -6,6 +6,6 @@ module CartsHelper
 
   def deliver_tip_text(total_price)
     return '' if total_price == 0
-    "*总价已含运费(普通快递)，bong 系列产品需要收取额外运费，统一发圆通快递。"
+    "* 总价已含运费(普通快递)。bong 系列产品需要收取额外运费，并使用固定的快递方式。"
   end
 end
