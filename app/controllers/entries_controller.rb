c030b10996b7227bb62af749f4095e2460d57bf8
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-12-09 20:53:27 +0800

    Fix #1092

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,8 +4,8 @@ class EntriesController < ApplicationController
 
   def show
     @entry = Entry.find(params[:id])
-    redirect_to root_path if !@entry.published? && !current_user.try(:role?, :editor)
-    redirect_to @entry.external_link if @entry.external_link.present?
+    return redirect_to root_path if !@entry.published? && !current_user.try(:role?, :editor)
+    return redirect_to @entry.external_link if @entry.external_link.present?
     respond_to do |format|
       format.wechat do
         render wechat: @entry, layout: false
