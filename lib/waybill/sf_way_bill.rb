c9a949d822d81219a38003ba6626e38b1a94db25
jasl <jasl9187@hotmail.com>
2014-12-12 16:02:41 +0800

    new sf waybill layout

--- lib/waybill/sf_way_bill.rb
+++ lib/waybill/sf_way_bill.rb
@@ -7,43 +7,49 @@ class SfWayBill < WayBill
   end
 
   def draw_body_from_order(order)
-    set_text(310, 440, COMPANY)
-    set_text(980, 440, SENDER)
+    set_text(380, 360, COMPANY)
+    set_text(980, 360, SENDER)
 
     addr1 = ADDRESS
-    set_text(310, 600, addr1[0..16])
-    set_text(310, 680, addr1[17..-1])
+    set_text(380, 510, addr1[0..16])
+    set_text(380, 580, addr1[17..-1])
 
-    set_text(310, 520, PHONE)
+    set_text(380, 440, PHONE)
 
     # =====
 
     contact = order.address
 
-    set_text(980, 860, contact.name)
+    set_text(980, 740, contact.name)
 
     addr2 = "#{contact.province} #{contact.city} #{contact.district} #{contact.street}"
-    set_text(310, 1000, addr2[0..16])
-    set_text(310, 1100, addr2[17..-1])
+    set_text(330, 890, addr2[0..16])
+    set_text(330, 960, addr2[17..-1])
 
-    set_text(830, 950, contact.phone)
+    set_text(830, 830, contact.phone)
 
     # =====
 
-    base_y = 1180
+    base_y = 1060
     order.order_items.each do |i|
-      set_text(210, base_y, "#{i.thing_title}x#{i.quantity}")
+      set_text(260, base_y, "#{i.thing_title}x#{i.quantity}")
       base_y += 48
     end
 
     # =====
 
-    set_text(1650, 1080, ACCOUNT)
+    set_text(1600, 940, ACCOUNT)
 
     # =====
 
-    set_text(1430, 1300, Time.now.month)
-    set_text(1550, 1300, Time.now.day)
+    set_text(1460, 1080, SENDER)
+
+    set_text(1400, 1140, Time.now.month)
+    set_text(1500, 1140, Time.now.day)
+
+    # =====
+
+    set_text(1740, 220, 'AT755')
 
     self
   end
