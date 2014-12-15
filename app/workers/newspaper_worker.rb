0475fd4bb753ae8715e07b85d4abb8017ce95a1c
jasl <jasl9187@hotmail.com>
2014-12-16 03:06:45 +0800

    retry newspaper worker

--- app/workers/newspaper_worker.rb
+++ app/workers/newspaper_worker.rb
@@ -1,6 +1,6 @@
 class NewspaperWorker
   include Sidekiq::Worker
-  sidekiq_options :queue => :newspaper, :backtrace => true, retry: false
+  sidekiq_options :queue => :newspaper, :backtrace => true
 
   def perform(user_id, date = Date.today)
     UserMailer.newspaper(user_id, date).deliver
