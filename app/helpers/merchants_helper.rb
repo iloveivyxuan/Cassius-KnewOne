87b62c1dc3dd3ec90c8ae97ffa79ddd0415f0319
liamzhao <sethverlo5@gmail.com>
2014-12-17 12:29:46 +0800

    clean up code

--- app/helpers/merchants_helper.rb
+++ app/helpers/merchants_helper.rb
@@ -7,4 +7,22 @@ module MerchantsHelper
       '链接'
     end
   end
+
+  def merchant_service_type(merchant)
+    return 'script' unless merchant
+    merchant.customer_service_type
+  end
+
+  def placeholder(type='script')
+    if type == "script"
+      '格式：<script async="true" src="https://s.meiqia.com/js/mechat.js?unitid=4075"></script>'
+    else
+      "格式：http://bong.cn/service"
+    end
+  end
+
+  def customer_service_value(merchant)
+    return "" unless merchant
+    merchant.customer_service
+  end
 end
