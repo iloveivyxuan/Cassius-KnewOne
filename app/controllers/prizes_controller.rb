a5e94eb9d9b8f0e94c71c47e372246b964a724f3
liamzhao <sethverlo5@gmail.com>
2014-12-26 15:22:01 +0800

    fix page number

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -2,8 +2,8 @@ class PrizesController < ApplicationController
   def index
     @prizes = Prize.all.valid
     params[:page] ||= 1
-    since_date = params[:page].day.ago.to_date
-    due_date = (params[:page] * 5).day.ago.to_date
+    since_date = (5 * (params[:page].to_i - 1) + 1).day.ago.to_date
+    due_date = (params[:page].to_i * 5).day.ago.to_date
     @prizes = @prizes.gte(due: due_date).lte(since: since_date)
   end
 end
