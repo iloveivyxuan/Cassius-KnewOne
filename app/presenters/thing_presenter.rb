aefd21df5deaf1f813d00ea439e96c86d810e1b0
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2014-12-29 14:18:16 +0800

    Humanize fanciers count and owners count

--- app/presenters/thing_presenter.rb
+++ app/presenters/thing_presenter.rb
@@ -163,7 +163,7 @@ class ThingPresenter < PostPresenter
   end
 
   def owners_count
-    content_tag :span, thing.owners_count, class: "owners_count"
+    content_tag :span, number_to_human(thing.owners_count), class: "owners_count"
   end
 
   def owners
@@ -259,7 +259,7 @@ class ThingPresenter < PostPresenter
   end
 
   def fanciers_count
-    content_tag :span, thing.fanciers_count, class: "fanciers_count"
+    content_tag :span, number_to_human(thing.fanciers_count), class: "fanciers_count"
   end
 
   def share_content
