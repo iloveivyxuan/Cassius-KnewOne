2ac81bc9801254122ccada976a56f0777dfb2e02
jasl <jasl9187@hotmail.com>
2015-01-06 07:44:37 +0800

    password controller support Ajax, but NYI

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,4 +1,19 @@
 class PasswordsController < Devise::PasswordsController
   layout 'oauth'
   skip_before_action :require_not_blocked
+
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    respond_to do |format|
+      if @success = successfully_sent?(resource)
+        format.html { redirect_back_or root_path }
+        format.js
+      else
+        format.html { render 'new' }
+        format.js
+      end
+    end
+  end
 end
