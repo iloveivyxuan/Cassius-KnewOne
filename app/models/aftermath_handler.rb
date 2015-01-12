2aaea9924ad9c5ca1bd2436c74f84fb0683afc42
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2015-01-12 20:27:56 +0800

    Unbind fancy and list

--- app/models/aftermath_handler.rb
+++ app/models/aftermath_handler.rb
@@ -16,10 +16,6 @@ class AftermathHandler
 
     def thing_unfancy(thing, user)
       user.inc fancies_count: -1 if user.fancies_count > 0
-
-      user.thing_lists.each do |list|
-        list.items.where(thing: thing).destroy
-      end
     end
 
     def thing_own(thing, user)
--- spec/models/thing_list_spec.rb
+++ spec/models/thing_list_spec.rb
@@ -22,26 +22,9 @@ describe ThingList, type: :model do
       end
     end
 
-    context 'after create' do
-      it 'gets fancied' do
-        expect(item.thing.fancied?(thing_list.author)).to be true
-      end
-    end
-
     context 'after thing destroy' do
       before do
         item.thing.destroy
-        thing_list.reload
-      end
-
-      it 'destroys' do
-        expect(thing_list.items).to_not include item
-      end
-    end
-
-    context 'after unfancy thing' do
-      before do
-        item.thing.unfancy(thing_list.author)
         thing_list.reload
       end
 
