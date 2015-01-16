b3d64f2d8e78eef1ce41279b38c5ea6ab3376f75
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2015-01-16 21:51:50 +0800

    Refactor with responder

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -2,24 +2,21 @@ class ImpressionsController < ApplicationController
   prepend_before_action :require_signed_in
   load_resource :thing
 
+  respond_to :json
+
   def show
     @impression = @thing.impressions.find_or_initialize_by(author: current_user)
     @recent_tags = current_user.recent_tags(5)
     @popular_tags = @thing.popular_tags(5)
 
-    respond_to do |format|
-      format.json
-    end
+    respond_with @impression
   end
 
   def update
     @impression = @thing.impressions.find_or_create_by(author: current_user)
+    @impression.update(impression_params)
 
-    if @impression.update(impression_params)
-      format.json { head :no_content }
-    else
-      format.json { render json: @impression.errors, status: :unprocessable_entity }
-    end
+    respond_with @impression
   end
 
   private
