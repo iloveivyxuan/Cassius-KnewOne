eb54593a50b6bf202d22d34fe4de085334534c55
liamzhao <sethverlo5@gmail.com>
2015-01-23 15:22:10 +0800

    move to post helper

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -16,31 +16,4 @@ module EntriesHelper
     content_tag(:li, options) {yield}
   end
 
-  def load_entry_resources(post)
-    html_doc = Nokogiri::HTML.fragment(post.content)
-    html_doc.css('.knewone-embed:empty').each do |element|
-      type = element["data-knewone-embed-type"]
-      key = element["data-knewone-embed-key"]
-      options = element["data-knewone-embed-options"]
-      photos = JSON.parse(options)["photos"] if options
-
-      case type
-      when 'thing'
-        slugs = key.split(',')
-        things = Thing.any_in(slugs: slugs)
-        photos = options ? photos.split(',') : Array.new(things.size, "")
-        result = render partial: 'things/embed_thing', collection: things.zip(photos), locals: { klass: (slugs.size > 1) ? 'col-sm-6' : 'col-sm-12' }, as: 'embed'
-      when 'list'
-        list = ThingList.find key
-        result = render [list], layout: browser.desktop? ? :quintet : :grid
-      when 'review'
-        review = Review.find key
-        result = render partial: 'home/hot_review', collection: [review]
-      end
-
-      element.add_child(result)
-    end
-    html_doc.to_html
-  end
-
 end
