d51a2069876277f95dadcb9fa23cc7e8c0bbc163
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2015-01-27 01:41:24 +0800

    Fix order

--- app/models/concerns/ids_sortable.rb
+++ app/models/concerns/ids_sortable.rb
@@ -6,7 +6,7 @@ module IdsSortable
       class_eval <<-EVAL
         def #{f}_sorted_by_ids(page = 1, per = 24)
           page = page ? page.to_i : 1
-          field = self.send(:"#{f.to_s.singularize}_ids").uniq.reverse
+          field = self.send(:"#{f.to_s.singularize}_ids").uniq
           count = field.size
           offset = per * (page - 1)
           ranged = field[(offset)..(offset + per - 1)] || []
