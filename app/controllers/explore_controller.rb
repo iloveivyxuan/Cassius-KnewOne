21a0fc64662d3735f296e86b3c43f6c529c182d2
liamzhao <sethverlo5@gmail.com>
2015-01-31 11:38:28 +0800

    offset page

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,10 +1,10 @@
 class ExploreController < ApplicationController
   skip_before_action :require_not_blocked
   helper :entries
-  before_action :set_page
+  before_action :set_params
 
   def index
-    @entries = Entry.published.not.in(category: %w(活动 特写)).desc(:created_at).page(params[:page]).per(@per)
+    @entries = Entry.published.not.in(category: %w(活动 特写)).desc(:created_at).page(@page).per(@per).offset(@offset)
 
     respond_to do |format|
       format.html
@@ -23,14 +23,18 @@ class ExploreController < ApplicationController
   }.each do |k, v|
     class_eval <<-EVAL
         def #{k}
-          @entries = Entry.published.where(category: '#{v}').desc(:created_at).page(params[:page]).per(@per)
+          @entries = Entry.published.where(category: '#{v}').desc(:created_at).page(@page).per(@per).offset(@offset)
           render 'index'
         end
     EVAL
   end
 
-  def set_page
-    params[:page] ||= 1
-    @per = (params[:page].to_i > 1) ? 12 : 11
+  def set_params
+    default_per = 12
+    n = (params[:page].to_i > 0) ? params[:page].to_i : 1 # WTF..`nil.to_i == 0` ???????
+
+    @page   = n
+    @per    = (n == 1) ? default_per - 1 : default_per
+    @offset = (n == 1) ? 0 : default_per * (n - 1) - 1
   end
 end
