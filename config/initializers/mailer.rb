5629ca57b5a704fdfd3d8ced58236289c2fe1192
jasl <jasl9187@hotmail.com>
2015-02-03 11:26:10 +0800

    not really send email in development

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,4 +1,4 @@
-if true# !Rails.env.development?
+if !Rails.env.development?
   Making::Application.configure do
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
