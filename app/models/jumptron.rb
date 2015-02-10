8aeede1fc22ff80fc1abc4af41418b947f5a7599
aiasfina <aiasfina@hotmail.com>
2015-02-10 15:34:37 +0800

    give jumptron default value

--- app/models/jumptron.rb
+++ app/models/jumptron.rb
@@ -7,20 +7,13 @@ class Jumptron
   field :alt, type: String
   field :href, type: String
   field :default, type: Boolean, default: false
-  field :type, type: Symbol
+  field :type, type: Symbol, default: :normal
 
   mount_uploader :image, ImageUploader
 
   validates :image, presence: true, file_size: {maximum: 8.megabytes.to_i}
   validates :type, inclusion: {in: Jumptron::TYPE}
 
-  scope :by_type, -> (type) do
-    type_sym = type.to_sym
-    if type_sym == :normal
-      where(:type.in => [type_sym, nil])
-    else
-      where(type: type_sym)
-    end
-  end
+  scope :by_type, -> (type) { where(type: type) }
 
 end
