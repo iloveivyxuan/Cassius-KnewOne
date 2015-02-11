5b9ae822678c4613283c26b11356fefbc323059d
liamzhao <sethverlo5@gmail.com>
2015-02-11 11:29:20 +0800

    select by description

--- app/controllers/haven/thing_lists_controller.rb
+++ app/controllers/haven/thing_lists_controller.rb
@@ -3,20 +3,23 @@ module Haven
     layout 'settings'
 
     def index
+      @thing_lists = ::ThingList
       case params[:sort_by]
       when 'fanciers_count'
-        @thing_lists = ThingList.desc(:fanciers_count)
+        @thing_lists = @thing_lists.desc(:fanciers_count)
         @sort_by = 'fanciers_count'
       when 'created_at'
-        @thing_lists = ThingList.desc(:created_at)
+        @thing_lists = @thing_lists.desc(:created_at)
         @sort_by = 'created_at'
       when 'updated_at'
-        @thing_lists = ThingList.desc(:updated_at)
+        @thing_lists = @thing_lists.desc(:updated_at)
         @sort_by = 'updated_at'
       else
-        @thing_lists = ThingList.hot
+        @thing_lists = @thing_lists.hot
         @sort_by = 'heat'
       end
+
+      @thing_lists = @thing_lists.where(description: /#{params[:description]}/i) if params[:description]
 
       @thing_lists = @thing_lists.page(params[:page])
     end
