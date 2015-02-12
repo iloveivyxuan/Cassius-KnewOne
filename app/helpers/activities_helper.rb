35ee5aba9b537d8b209bea9d4f17379f62190c09
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2015-02-12 16:46:38 +0800

    Refactor

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -10,4 +10,17 @@ module ActivitiesHelper
 
     render "activities/#{tmpl}", activity: activity
   end
+
+  def action_of_activity(activity)
+    raw case activity.type
+        when :new_thing   then '发布'
+        when :own_thing   then '拥有'
+        when :fancy_thing then '喜欢'
+        when :new_review  then '发表'
+        when :love_review then '赞'
+        when :new_feeling then '发表了' + link_to('短评', [activity.reference.thing, activity.reference])
+        when :add_to_list then '加入' + link_to(activity.source.name, activity.source) + '列表'
+        when :fancy_list  then '喜欢该列表'
+        end
+  end
 end
