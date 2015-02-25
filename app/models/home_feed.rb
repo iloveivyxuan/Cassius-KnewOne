98feb370d1e43e030fc3cf9b1e5e9fb51729f6de
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2015-02-25 15:56:05 +0800

    Optimize

--- app/models/home_feed.rb
+++ app/models/home_feed.rb
@@ -3,7 +3,7 @@ class HomeFeed
 
   class << self
     def create_from_activities(activities)
-      activities.select(&:reference).uniq do |a|
+      activities.uniq do |a|
         [a.type, a.related_thing, a.related_thing_list, a.user_id]
       end.reduce({}) do |feeds, a|
         subject = a.related_thing || a.related_thing_list
