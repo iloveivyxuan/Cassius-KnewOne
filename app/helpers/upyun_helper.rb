b4c8eea2b71ae342dc459147606e10d4d77b6fac
Syn Zhang <zeyanzhang.china@gmail.com>
2015-02-27 20:09:16 +0800

    Add a directory param for UpyunHelpers

--- app/helpers/upyun_helper.rb
+++ app/helpers/upyun_helper.rb
@@ -1,9 +1,16 @@
 module UpyunHelper
-  def upyun_policy
+  def upyun_directory
+    {
+      default: "photos",
+      review: "review_photos"
+    }
+  end
+
+  def upyun_policy(type)
     raw = {
       "bucket" => Settings.upyun.photo_bucket,
       "expiration" => 3.hours.since.to_i,
-      "save-key" => "/review_photos/{filemd5}{.suffix}",
+      "save-key" => "/#{upyun_directory[type]}/{filemd5}{.suffix}",
       "allow-file-type" => "jpg,jpeg,gif,png",
       "content-length-range" => "0, #{4.megabyte}",
       "x-gmkerl-rotate" => "auto"
@@ -11,15 +18,15 @@ module UpyunHelper
     Base64.strict_encode64(raw.to_json)
   end
 
-  def upyun_signature
-    Digest::MD5.hexdigest "#{upyun_policy}&#{Settings.upyun.form_secret}"
+  def upyun_signature(type)
+    Digest::MD5.hexdigest "#{upyun_policy(type)}&#{Settings.upyun.form_secret}"
   end
 
-  def upyun_data
+  def upyun_data(type = :review)
     domain = "http://" + Settings.upyun.photo_bucket_domain
     {
-      policy: upyun_policy,
-      signature: upyun_signature,
+      policy: upyun_policy(type),
+      signature: upyun_signature(type),
       url: "http://v0.api.upyun.com/" + Settings.upyun.photo_bucket,
       domain: domain
     }
