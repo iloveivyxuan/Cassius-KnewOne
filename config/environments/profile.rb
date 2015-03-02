b88dc6615bea668df52a7ebd95e2a0f46478a66a
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2015-03-02 10:15:39 +0800

    Perform caching

--- config/environments/profile.rb
+++ config/environments/profile.rb
@@ -2,6 +2,7 @@ Making::Application.configure do
   config.eager_load = true
   config.cache_classes = true
   config.consider_all_requests_local = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.cache_store = :redis_store, {namespace: "KO", expires_in: 1.hour}
   config.action_dispatch.show_exceptions = true
 end
