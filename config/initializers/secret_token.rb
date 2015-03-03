1f301c273017fbf8957b90e5fdf055ac3a672c09
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2015-03-03 21:59:27 +0800

    Use the new secrets.yml convention to store your secret keys

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Making::Application.config.secret_token = 'cf9f05f6cbe96a3cd8aff869e12edd3c276400b915c3c4c7081e4a2b5eb9a20d632c3a61ab13c5c654d44f41999b86b3fe4edae1a4d61056767a4f5041061dbf'
