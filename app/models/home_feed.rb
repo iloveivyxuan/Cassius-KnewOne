1d44723f55a4ddc8e852b2ee3b3ac698704399c3
Sun Zheng'an (孙正安) <sun.zheng.an.0@gmail.com>
2015-03-04 11:15:45 +0800

    Hide the fancy_thing activities followed by own_thing

--- app/models/home_feed.rb
+++ app/models/home_feed.rb
@@ -46,7 +46,7 @@ class HomeFeed
       @reviews << review unless @reviews.include?(review)
     end
 
-    return if activity.type == :fancy_thing && @activities.any? { |a| a.type == :desire_thing }
+    return if activity.type == :fancy_thing && @activities.any? { |a| [:desire_thing, :own_thing].include?(a.type) }
     return if activity.type == :desire_thing && @activities.any? { |a| a.type == :own_thing }
 
     @activities << activity
