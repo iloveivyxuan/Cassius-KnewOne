f78c5704fd38da01fc9e40b60b91b8fe9c98a5eb
jasl <jasl9187@hotmail.com>
2015-03-05 18:32:08 +0800

    lazy evalute puma deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,12 +16,12 @@ set :bundle_bins, %w(gem rake rails whenever)
 set :whenever_roles, :app
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_access.log"
-set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :puma_state, -> { "#{shared_path}/tmp/pids/puma.state" }
+set :puma_pid, -> { "#{shared_path}/tmp/pids/puma.pid" }
+set :puma_bind, -> { "unix://#{shared_path}/tmp/sockets/puma.sock" }
+set :puma_conf, -> { "#{shared_path}/puma.rb" }
+set :puma_access_log, -> { "#{shared_path}/log/puma_access.log" }
+set :puma_error_log, -> { "#{shared_path}/log/puma_error.log" }
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [4, 16]
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,3 +5,6 @@ set :branch, 'staging'
 set :rails_env, 'staging'
 
 set :ssh_options, { port: 22222, forward_agent: true }
+
+set :puma_threads, [2, 8]
+set :puma_workers, 2
