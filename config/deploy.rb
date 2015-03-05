8622b25df8f7103c9e90ea16c6aaa92ebf253523
jasl <jasl9187@hotmail.com>
2015-03-05 18:41:11 +0800

    lazy evalute puma env

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :puma_conf, -> { "#{shared_path}/puma.rb" }
 set :puma_access_log, -> { "#{shared_path}/log/puma_access.log" }
 set :puma_error_log, -> { "#{shared_path}/log/puma_error.log" }
 set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_env, -> { fetch(:rack_env, fetch(:rails_env, 'production')) }
 set :puma_threads, [4, 16]
 set :puma_workers, 4
 set :puma_worker_timeout, 60
